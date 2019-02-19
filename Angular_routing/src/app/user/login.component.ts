832d54a76bc79cf32138a8023bbac425b433045c
jackhwl <jackhwl@hotmail.com>
2019-02-19 07:38:30 -0500

    Routing to Features

--- Angular_routing/src/app/user/login.component.ts
+++ Angular_routing/src/app/user/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html'
@@ -10,7 +11,7 @@ export class LoginComponent {
   errorMessage: string;
   pageTitle = 'Log In';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   login(loginForm: NgForm) {
     if (loginForm && loginForm.valid) {
@@ -19,6 +20,7 @@ export class LoginComponent {
       this.authService.login(userName, password);
 
       // Navigate to the Product List page after log in.
+      this.router.navigate(['/products']);
     } else {
       this.errorMessage = 'Please enter a user name and password.';
     }
