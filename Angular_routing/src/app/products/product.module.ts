42cec3f6790b93a7440ecb1dbf9e138022302d88
jackhwl <jackhwl@hotmail.com>
2019-02-24 18:33:59 -0500

    Grouping and Component-less Routes

--- Angular_routing/src/app/products/product.module.ts
+++ Angular_routing/src/app/products/product.module.ts
@@ -15,17 +15,21 @@ import { ProductEditTagsComponent } from './product-edit/product-edit-tags.compo
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent, resolve: { resolvedData: ProductResolver} },
-      { path: 'products/:id/edit', 
-        component: ProductEditComponent, 
-        resolve: { resolvedData: ProductResolver},
+      { path: 'products', 
         children: [
-          { path: '', redirectTo: 'info', pathMatch: 'full' },
-          { path: 'info', component: ProductEditInfoComponent },
-          { path: 'tags', component: ProductEditTagsComponent }
-        ]
-      },
+          { path: '', component: ProductListComponent },
+          { path: ':id', component: ProductDetailComponent, resolve: { resolvedData: ProductResolver} },
+          { path: ':id/edit', 
+            component: ProductEditComponent, 
+            resolve: { resolvedData: ProductResolver},
+            children: [
+              { path: '', redirectTo: 'info', pathMatch: 'full' },
+              { path: 'info', component: ProductEditInfoComponent },
+              { path: 'tags', component: ProductEditTagsComponent }
+            ]
+          }
+        ] 
+      }
     ])
   ],
   declarations: [
