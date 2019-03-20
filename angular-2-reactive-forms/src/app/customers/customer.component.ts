f7fc768ba8f5d149f5dacb5ed7c26ab2fa25320e
jack <jhuang@olapvision.com>
2019-03-20 11:39:42 -0400

    Setting Built-in Validation Rules

--- angular-2-reactive-forms/src/app/customers/customer.component.ts
+++ angular-2-reactive-forms/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -16,9 +16,9 @@ export class CustomerComponent implements OnInit {
 
   ngOnInit() {
     this.customerForm = this.fb.group({
-      firstName: '',
-      lastName: '', //{value: 'n/a', disabled: true}
-      email: '',
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]], //{value: 'n/a', disabled: true}
+      email: ['', [Validators.required, Validators.email]],
       sendCatalog: true
     });
   }
