eb91f9d81a3bd22541cda27dc1913c1b62fa29bc
jack <jackhwl@hotmail.com>
2019-03-21 21:43:16 -0400

    Loop Through the FormArray, Duplicate the Input Elements

--- angular-2-reactive-forms/src/app/customers/customer.component.ts
+++ angular-2-reactive-forms/src/app/customers/customer.component.ts
@@ -39,7 +39,7 @@ export class CustomerComponent implements OnInit {
   get addresses(): FormArray{
     return <FormArray>this.customerForm.get('addresses');
   }
-  
+
   private validationMessages = {
     required: 'Please enter your email address.',
     email: 'Please enter a valid email address.'
@@ -69,6 +69,10 @@ export class CustomerComponent implements OnInit {
     emailControl.valueChanges
       .pipe(debounceTime(1000))
       .subscribe(value => this.setMessage(emailControl));
+  }
+
+  addAddress(): void {
+    this.addresses.push(this.buildAddress());
   }
 
   buildAddress(): FormGroup {
