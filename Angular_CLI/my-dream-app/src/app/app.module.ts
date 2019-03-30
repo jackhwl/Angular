e2aa52faf8bb15cba5f322dcfff150a3fdb1fe26
jackhwl <jackhwl@hotmail.com>
2019-03-30 10:55:37 -0400

    generating a new moudle with routing

--- Angular_CLI/my-dream-app/src/app/app.module.ts
+++ Angular_CLI/my-dream-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomerComponent } from './customer/customer.component';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CustomerComponent } from './customer/customer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
