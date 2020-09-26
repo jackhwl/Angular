9ae654d9b09c125a52d66668f090ba5bd0b28253
Wenlin Huang <jackhwl@hotmail.com>
2020-09-26 10:02:07 -0400

    Adding Angular Material

--- angular-workshop/src/app/app.module.ts
+++ angular-workshop/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemsComponent } from './items/items.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
