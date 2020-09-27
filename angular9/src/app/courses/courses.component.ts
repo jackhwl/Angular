ae2950928b2f5e2bea03578caa5de66e8972f244
Wenlin Huang <jackhwl@hotmail.com>
2020-09-27 16:51:17 -0400

    Component Fundamentals Exercise

--- angular9/src/app/courses/courses.component.ts
+++ angular9/src/app/courses/courses.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+  currentCourse = null;
   courses = [
     {
       id: 1,
@@ -13,12 +14,24 @@ export class CoursesComponent implements OnInit {
       description: 'Learn the fundamentals of Angular 9',
       percentComplete: 26,
       favorite: true
+    },
+    {
+      id: 2,
+      title: 'Git commit message Best Practise',
+      description: 'Learn the fundamentals of Git commit message',
+      percentComplete: 62,
+      favorite: true
     }
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectCourse(course) {
+    console.log(course);
+    this.currentCourse = course;
   }
 
 }
