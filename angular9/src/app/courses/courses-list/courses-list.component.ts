e15d3eb5b13fb9f6206f97f56ecedb2145fcdac9
Wenlin Huang <jackhwl@hotmail.com>
2020-10-01 21:41:15 -0400

    Component Driven Architecture

---
+++ angular9/src/app/courses/courses-list/courses-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-list',
+  templateUrl: './courses-list.component.html',
+  styleUrls: ['./courses-list.component.scss']
+})
+export class CoursesListComponent implements OnInit {
+  courses = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
