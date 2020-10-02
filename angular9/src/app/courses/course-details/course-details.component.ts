3fea7b486aebc956051a738a16887b85fcd8fdc0
Wenlin Huang <jackhwl@hotmail.com>
2020-10-01 22:13:27 -0400

    Component API Input & Output

--- angular9/src/app/courses/course-details/course-details.component.ts
+++ angular9/src/app/courses/course-details/course-details.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
   styleUrls: ['./course-details.component.scss']
 })
-export class CourseDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CourseDetailsComponent {
+  @Input() course;
+  @Output() saved = new EventEmitter();
+  @Output() cancelled = new EventEmitter();
 }
--- angular9/src/app/courses/courses-list/courses-list.component.ts
+++ angular9/src/app/courses/courses-list/courses-list.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
   styleUrls: ['./courses-list.component.scss']
 })
-export class CoursesListComponent implements OnInit {
-  courses = []
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CoursesListComponent {
+  @Input() courses;
+  @Output() selected = new EventEmitter();
+  @Output() deleted = new EventEmitter();
 }
