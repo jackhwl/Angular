59c54762fb1cebd5320c7f6cccf155f052d46a98
Wenlin Huang <jackhwl@hotmail.com>
2020-10-02 10:55:42 -0400

    Component Architecture Solution

---
+++ angular9/src/app/home/lessons-list/lessons-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-lessons-list',
+  templateUrl: './lessons-list.component.html',
+  styleUrls: ['./lessons-list.component.scss']
+})
+export class LessonsListComponent {
+  @Input() lessons;
+  @Output() selected = new EventEmitter();
+}
