0572a6f3bcb3944671419b4ebc5ae437f67500d2
Wenlin Huang <jackhwl@hotmail.com>
2020-10-03 14:17:13 -0400

    Creating a Lazy Loaded Route

--- angular9/src/app/app-routing.module.ts
+++ angular9/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'courses', component: CoursesComponent },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: '**', redirectTo: '/home' }
 ];
 
