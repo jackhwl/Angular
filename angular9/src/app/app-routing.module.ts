f643324fb365a7432e7dc7eb2b1dcbb0d7246f57
Wenlin Huang <jackhwl@hotmail.com>
2020-10-03 15:23:30 -0400

    Create a Route Solution

--- angular9/src/app/app-routing.module.ts
+++ angular9/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CoursesComponent } from './courses/courses.component';
 import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'courses', component: CoursesComponent },
+  { path: 'users', component: UsersComponent },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: '**', redirectTo: '/home' }
 ];
