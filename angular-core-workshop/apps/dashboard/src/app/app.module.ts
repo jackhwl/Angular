599a0489b0ffe917638c0d3b7045d4c160f507ed
Wenlin Huang <jackhwl@hotmail.com>
2020-10-04 21:57:17 -0400

    Angular Material

--- angular-core-workshop/apps/dashboard/src/app/app.module.ts
+++ angular-core-workshop/apps/dashboard/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@workshop/material';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # Angular
 # angular-core-workshop
+ * https://github.com/onehungrymind/angular-core-workshop/wiki/00-Up-and-Running
  * npm install -g @angular/cli
  * npm install -g @nrwl/schematics
  * npx create-nx-workspace angular-core-workshop --preset=empty --cli=angular --npmScope=workshop
@@ -7,4 +8,4 @@
  * ng config schematics.@nrwl/schematics:component.styleext scss
  * npm install --save-dev @nrwl/angular
  * ng g @nrwl/angular:app dashboard --routing -p=app --style=scss --d
- 
+ * ng add @angular/material
