655a575f2a141d5fa5b953f5b6e5c7e9ef6a32e5
Wenlin Huang <jackhwl@hotmail.com>
2020-10-07 09:10:29 -0400

    use create-store-libs.sh to generate components

--- bghoard/jest.config.js
+++ bghoard/jest.config.js
@@ -9,5 +9,11 @@ module.exports = {
     '<rootDir>/libs/review/feature-list',
     '<rootDir>/libs/review/feature-details',
     '<rootDir>/apps/store',
+    '<rootDir>/libs/store/feature-list',
+    '<rootDir>/libs/store/feature-details',
+    '<rootDir>/libs/store/feature-cart',
+    '<rootDir>/libs/store/ui-formatters',
+    '<rootDir>/libs/store/data-access-games',
+    '<rootDir>/libs/store/data-access-cart',
   ],
 };
---
+++ bghoard/libs/store/data-access-cart/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  displayName: 'store-data-access-cart',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
+  coverageDirectory: '../../../coverage/libs/store/data-access-cart',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
