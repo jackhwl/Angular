50a6d3bf1e72f9521dea41e9a73a71713ba4687b
Wenlin Huang <jackhwl@hotmail.com>
2020-10-07 21:18:08 -0400

    Adding the Application Shell

--- angular-core-workshop/apps/dashboard/src/app/projects/projects.module.ts
+++ angular-core-workshop/apps/dashboard/src/app/projects/projects.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
-
+import { MaterialModule } from '@workshop/material';
 
 @NgModule({
   declarations: [ProjectsComponent],
-  imports: [
-    CommonModule,
-    ProjectsRoutingModule
-  ]
+  imports: [CommonModule, ProjectsRoutingModule, MaterialModule],
+  exports: [ProjectsComponent],
 })
-export class ProjectsModule { }
+export class ProjectsModule {}
