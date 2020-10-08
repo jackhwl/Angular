d46a5717d6952a6d15efb5353370af8c65280e52
Wenlin Huang <jackhwl@hotmail.com>
2020-10-08 12:21:33 -0400

    ViewChild query Dom

--- angular-core-workshop/apps/dashboard/src/app/app.component.ts
+++ angular-core-workshop/apps/dashboard/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('sidenav') localSideNav;
   title = 'Angular Core Workshop';
+
+  toggle() {
+    this.localSideNav.toggle();
+  }
 }
