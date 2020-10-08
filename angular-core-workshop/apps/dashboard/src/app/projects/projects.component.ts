5442bae99e7141cbd06770e365929076615398b9
Wenlin Huang <jackhwl@hotmail.com>
2020-10-08 09:39:19 -0400

    Binding Component Data to Templates

--- angular-core-workshop/apps/dashboard/src/app/projects/projects.component.ts
+++ angular-core-workshop/apps/dashboard/src/app/projects/projects.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  projects = [
+    {
+      id: '1',
+      title: 'Project One',
+      details: 'This is a sample project',
+      percentComplete: 20,
+      approved: false,
+    },
+    {
+      id: '2',
+      title: 'Project Two',
+      details: 'This is a sample project',
+      percentComplete: 40,
+      approved: false,
+    },
+    {
+      id: '3',
+      title: 'Project Three',
+      details: 'This is a sample project',
+      percentComplete: 100,
+      approved: true,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
