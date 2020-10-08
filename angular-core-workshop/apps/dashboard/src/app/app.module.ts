31d2e4b77efddaf7ccafb6762c22976e2d7fc866
Wenlin Huang <jackhwl@hotmail.com>
2020-10-07 20:35:42 -0400

    Generate Modules home projects customers

--- angular-core-workshop/apps/dashboard/src/app/app.module.ts
+++ angular-core-workshop/apps/dashboard/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '@workshop/material';
 
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { ProjectsModule } from './projects/projects.module';
+import { CustomersModule } from './customers/customers.module';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -13,6 +16,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     BrowserAnimationsModule,
     MaterialModule,
+    HomeModule,
+    ProjectsModule,
+    CustomersModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
