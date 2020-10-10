b51a48144869a63133f2dcd07800dda12f538050
Wenlin Huang <jackhwl@hotmail.com>
2020-10-10 15:24:46 -0400

    List Components

--- angular-core-workshop/apps/dashboard/src/app/projects/projects-list/projects-list.component.ts
+++ angular-core-workshop/apps/dashboard/src/app/projects/projects-list/projects-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Project } from "@workshop/core-data";
 
 @Component({
   selector: 'app-projects-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects-list.component.scss']
 })
 export class ProjectsListComponent implements OnInit {
-
+  @Input() projects: Project[];
+  @Input() readonly: false;
+  
   constructor() { }
 
   ngOnInit(): void {
