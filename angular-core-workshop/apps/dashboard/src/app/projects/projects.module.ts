3c7395cfe32e0f6c53d129252c91474ff9d3c45a
Wenlin Huang <jackhwl@hotmail.com>
2020-10-10 15:01:23 -0400

    Refactor App Into Components

--- angular-core-workshop/apps/dashboard/src/app/projects/projects.module.ts
+++ angular-core-workshop/apps/dashboard/src/app/projects/projects.module.ts
@@ -5,9 +5,11 @@ import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
 import { MaterialModule } from '@workshop/material';
 import { FormsModule } from '@angular/forms';
+import { ProjectsListComponent } from './projects-list/projects-list.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 @NgModule({
-  declarations: [ProjectsComponent],
+  declarations: [ProjectsComponent, ProjectsListComponent, ProjectDetailsComponent],
   imports: [ CommonModule, ProjectsRoutingModule, MaterialModule, FormsModule ],
   exports: [ProjectsComponent],
 })
