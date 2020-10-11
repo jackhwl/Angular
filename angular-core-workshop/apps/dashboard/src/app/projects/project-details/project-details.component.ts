650b4be748a34acd9d8435dd0c91c3d00e845a09
Wenlin Huang <jackhwl@hotmail.com>
2020-10-10 22:33:14 -0400

    Details Component

--- angular-core-workshop/apps/dashboard/src/app/projects/project-details/project-details.component.ts
+++ angular-core-workshop/apps/dashboard/src/app/projects/project-details/project-details.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Project } from "@workshop/core-data";
 
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
   styleUrls: ['./project-details.component.scss']
 })
-export class ProjectDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ProjectDetailsComponent {
+  @Input() project: Project;
+  @Output() saved = new EventEmitter();
+  @Output() cancelled = new EventEmitter();
 
 }
