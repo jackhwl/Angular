b4cbdded6892e2adcff2f802e8efae4fbde05f1d
Wenlin Huang <jackhwl@hotmail.com>
2020-10-11 17:30:35 -0400

    project component test

--- angular-core-workshop/apps/dashboard/src/app/projects/projects.component.spec.ts
+++ angular-core-workshop/apps/dashboard/src/app/projects/projects.component.spec.ts
@@ -1,33 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MaterialModule } from '@workshop/material';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ProjectDetailsComponent } from './project-details/project-details.component';
 import { ProjectsListComponent } from './projects-list/projects-list.component';
 
 import { ProjectsComponent } from './projects.component';
+import { DebugElement } from '@angular/core';
 
 describe('ProjectsComponent', () => {
-  let component: ProjectsComponent;
+  // Create my local test members
+  let compnent: ProjectsComponent;
   let fixture: ComponentFixture<ProjectsComponent>;
+  let de: DebugElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ProjectsComponent, ProjectsListComponent, ProjectDetailsComponent ],
-      imports: [ MaterialModule, FormsModule, HttpClientModule, BrowserAnimationsModule]
-    })
-    .compileComponents();
-  });
-
+  // Instantiate test bed
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProjectsComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.configureTestingModule({
+      declarations: [
+        ProjectsComponent, 
+        ProjectsListComponent, 
+        ProjectDetailsComponent
+      ],
+      imports: [
+        MaterialModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule
+      ]
+    }).createComponent(ProjectsComponent);
+
+    compnent = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should have a primaryColor of `red`', () => {
+    expect(compnent.primaryColor).toBe('red');
+  })
 });
