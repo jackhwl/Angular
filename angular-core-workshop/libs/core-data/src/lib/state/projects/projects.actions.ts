775e4dc2c338418a0ac76041216aa7457b3f0b0d
Wenlin Huang <jackhwl@hotmail.com>
2020-10-26 11:30:54 -0400

    Side Effects & Server Calls

--- angular-core-workshop/libs/core-data/src/lib/state/projects/projects.actions.ts
+++ angular-core-workshop/libs/core-data/src/lib/state/projects/projects.actions.ts
@@ -4,7 +4,11 @@ import { Project } from "../../projects/project";
 export enum ProjectsActionTypes {
     ProjectSelected = '[Projects] Seleted',
     LoadProjects = '[Projects] Load Data',
+    ProjectsLoaded = '[Projects] Data Loaded',
+    
     AddProject = '[Projects] Add Data',
+    ProjectsAdded = '[Projects] Data Added',
+
     UpdateProject = '[Projects] Update Data',
     DeleteProject = '[Projects] Delete Data',
 }
@@ -16,11 +20,20 @@ export class SelectProject implements Action {
 
 export class LoadProjects implements Action {
     readonly type = ProjectsActionTypes.LoadProjects;
-    constructor(private payload: Project[]) {}c
+}
+
+export class ProjectsLoaded implements Action {
+    readonly type = ProjectsActionTypes.ProjectsLoaded;
+    constructor(private payload: Project[]) {}
 }
 
 export class AddProject implements Action { 
     readonly type = ProjectsActionTypes.AddProject;
+    constructor(private payload: Project) {}
+}
+
+export class ProjectAdded implements Action { 
+    readonly type = ProjectsActionTypes.ProjectsAdded;
     constructor(private payload: Project) {}
 }
 
@@ -34,4 +47,10 @@ export class DeleteProject implements Action {
     constructor(private payload: Project) {}
 }
 
-export type ProjectActions = SelectProject | LoadProjects | AddProject | UpdateProject | DeleteProject;
+export type ProjectActions = SelectProject 
+    | LoadProjects 
+    | ProjectsLoaded 
+    | AddProject 
+    | ProjectAdded 
+    | UpdateProject 
+    | DeleteProject;
---
+++ angular-core-workshop/libs/core-data/src/lib/state/projects/projects.effects.ts
@@ -0,0 +1,29 @@
+// HELPFUL SNIPPET
+import { Injectable } from '@angular/core';
+import { Actions, Effect } from '@ngrx/effects';
+import { DataPersistence } from '@nrwl/nx';
+import { map } from 'rxjs/operators';
+
+import { Project } from './../../projects/project';
+import { ProjectsService } from './../../projects/projects.service';
+import { ProjectsActionTypes } from './projects.actions';
+import { ProjectsState } from './projects.reducer';
+
+@Injectable({providedIn: 'root'})
+export class ProjectsEffects {
+  @Effect() loadProjects$ = this.dataPersistence.fetch(ProjectsActionTypes.LoadProjects, {
+    run: () => {},
+    onError: () => {}
+  })
+
+  @Effect() addProjects$ = this.dataPersistence.pessimisticUpdate(ProjectsActionTypes.AddProject, {
+    run: () => {},
+    onError: () => {}
+  })
+
+  constructor(
+    private actions$: Actions,
+    private dataPersistence: DataPersistence<ProjectsState>,
+    private projectsService: ProjectsService
+  ) {}
+}
