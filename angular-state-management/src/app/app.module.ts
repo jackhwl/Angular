a88b6edb7a3acb5451a4b342e02bec95f1a8ca8a
Wenlin Huang <jackhwl@hotmail.com>
2020-11-10 10:06:58 -0500

    done

--- angular-state-management/src/app/app.module.ts
+++ angular-state-management/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import {CustomerModule} from './customer/customer.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : []
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    CustomerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
