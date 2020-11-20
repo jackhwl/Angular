5b283e9608f6716566b88981660505eb5f0db0fa
Wenlin Huang <jackhwl@hotmail.com>
2020-11-20 10:03:50 -0500

    students list component init

--- wl-app/apps/recruit/src/app/students/students-list/students-list.component.ts
+++ wl-app/apps/recruit/src/app/students/students-list/students-list.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Student } from '@wl/api-interfaces';
 
 @Component({
   selector: 'wl-students-list',
   templateUrl: './students-list.component.html',
   styleUrls: ['./students-list.component.scss']
 })
-export class StudentsListComponent implements OnInit {
 
+export class StudentsListComponent implements OnInit {
+  @Input() students: Student[]
   constructor() { }
 
   ngOnInit(): void {
