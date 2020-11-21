01ffaa54b0489ec5304affa28ecbe5266eb7155f
Wenlin Huang <jackhwl@hotmail.com>
2020-11-21 11:53:40 -0500

    use httpClient in studentsService

--- wl-app/apps/recruit/src/app/students/students.component.ts
+++ wl-app/apps/recruit/src/app/students/students.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Student } from '@wl/api-interfaces';
 import { StudentsService } from '@wl/core-data';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'wl-students',
@@ -9,13 +10,16 @@ import { StudentsService } from '@wl/core-data';
 })
 export class StudentsComponent implements OnInit {
   students: Student[];
-
   constructor(private studentsService: StudentsService) { }
 
   ngOnInit(): void {
-    this.students = this.studentsService.all();
+    this.getStudents();
   }
 
+  getStudents() {
+    this.studentsService.all()
+    .subscribe((result) => this.students = result);
+  }
   selectedStudent(student) {
     console.log(student);
   }
--- wl-app/libs/core-data/src/lib/services/students/students.service.ts
+++ wl-app/libs/core-data/src/lib/services/students/students.service.ts
@@ -1,75 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '@env/environment';
 import { Student } from '@wl/api-interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentsService {
-  private students: Student[] =[
-    {
-      id: "1",
-      firstName: "Alexander",
-      middleName: "D",
-      lastName: "Russo",
-      email: "arusso1@uga.edu",
-      schoolName: "University of Georgia, School of Law",
-      year: "2L",
-      graduationDate: "2016-05-01"
-    },
-    {
-      id: "2",
-      firstName: "Greg",
-      middleName: null,
-      lastName: "Djordjevic",
-      email: "djordjevic.6@osu.edu",
-      schoolName: "(Removed School)",
-      year: "2L",
-      graduationDate: "-0001-11-30"
-    },
-    {
-      id: "3",
-      firstName: "Cory",
-      middleName: "L ",
-      lastName: "Takeuchi",
-      email: "coryt@uga.edu",
-      schoolName: "University of Georgia, School of Law",
-      year: "2L",
-      graduationDate: "2016-05-01"
-    },
-    {
-      id: "4",
-      firstName: "Alexander",
-      middleName: "D",
-      lastName: "Russo",
-      email: "arusso1@uga.edu",
-      schoolName: "University of Georgia, School of Law",
-      year: "2L",
-      graduationDate: "2016-05-01"
-    },
-    {
-      id: "5",
-      firstName: "Tabitha",
-      middleName: "Ashley ",
-      lastName: "Ferrer",
-      email: "tabbyferrer@yahoo.com",
-      schoolName: "Howard University, School of Law",
-      year: "Grad",
-      graduationDate: "2014-05-01"
-    },
-    {
-      id: "6",
-      firstName: "Cindy",
-      middleName: "C.",
-      lastName: "Unegbu",
-      email: "CCUnegbu@law.howard.edu",
-      schoolName: "Howard University, School of Law",
-      year: "Grad",
-      graduationDate: "2014-05-01"
-    }
-  ]
-  constructor() { }
+  model = 'students';
+  private students;
+  constructor(private httpClient: HttpClient) { }
 
+  getUrl() {
+    return `${environment.apiEndpoint}${this.model}`;
+  }
   all() {
-    return this.students;
+    return this.httpClient.get<Student[]>(this.getUrl());
   }
 }
