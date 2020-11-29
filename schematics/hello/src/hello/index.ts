e657eb0f486dd063220e493f162a220fb889c55c
Wenlin Huang <jackhwl@hotmail.com>
2020-11-29 11:37:26 -0500

    add function to schmeatics' template

--- schematics/hello/src/hello/index.ts
+++ schematics/hello/src/hello/index.ts
@@ -1,14 +1,28 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, apply, mergeWith, template, url } from '@angular-devkit/schematics';
+
+import { strings } from '@angular-devkit/core';
+
 import { Schema } from './schema';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function hello(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    const { name } = _options;
+    
+    const sourceTemplates = url('./files');
 
-    tree.create('hello.js', `console.log('Hello ${name}!');`);
+    const sourceParameterizedTemplates = apply(sourceTemplates, [
+      template({
+        ..._options,
+        ...strings,
+        addExclamation
+      })
+    ]);
 
-    return tree;
+    return mergeWith(sourceParameterizedTemplates)(tree, _context);
   };
+}
+
+function addExclamation(value: string):string {
+  return value + '!';
 }
