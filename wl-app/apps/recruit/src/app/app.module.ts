1dbf19e6b99ded59a3013e66482182d517253d91
Wenlin Huang <jackhwl@hotmail.com>
2020-12-10 18:28:09 -0500

    slogan component added

--- wl-app/apps/recruit/src/app/app.module.ts
+++ wl-app/apps/recruit/src/app/app.module.ts
@@ -15,9 +15,10 @@ import { HomeComponent } from './home/home.component';
 import { StudentDetailsComponent } from './students/student-details/student-details.component';
 import { FormsModule } from '@angular/forms';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { SloganComponent } from './slogan/slogan.component';
 
 @NgModule({
-  declarations: [AppComponent, StudentsComponent, StudentsListComponent, HomeComponent, StudentDetailsComponent, HeaderComponent, SidenavListComponent],
+  declarations: [AppComponent, StudentsComponent, StudentsListComponent, HomeComponent, StudentDetailsComponent, HeaderComponent, SidenavListComponent, SloganComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
