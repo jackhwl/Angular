efe650656c79a2c47780643e70f6963fd74994df
Wenlin Huang <jackhwl@hotmail.com>
2020-12-17 19:18:54 -0500

    login call webApi

--- wl-app/apps/security/src/app/security/security.service.ts
+++ wl-app/apps/security/src/app/security/security.service.ts
@@ -3,13 +3,21 @@ import { AppUserAuth } from './app-user-auth';
 import { AppUser } from './app-user';
 import { LOGIN_MOCKS } from './login-mocks';
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
+const API_URL = "http://localhost:5000/api/security/";
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
 
 @Injectable()
 export class SecurityService {
   securityObject: AppUserAuth = new AppUserAuth();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   resetSecurityObject(): void {
     this.securityObject.userName = "";
@@ -25,11 +33,23 @@ export class SecurityService {
 
   login(entity: AppUser): Observable<AppUserAuth> {
     this.resetSecurityObject();
-    Object.assign(this.securityObject, LOGIN_MOCKS.find(user => user.userName.toLowerCase() === entity.userName.toLowerCase()));
-    if (this.securityObject.userName !== "") {
-        localStorage.setItem("bearerToken", this.securityObject.bearerToken);
-    }
-    return of<AppUserAuth>(this.securityObject);
+    // Object.assign(this.securityObject, LOGIN_MOCKS.find(user => user.userName.toLowerCase() === entity.userName.toLowerCase()));
+    // if (this.securityObject.userName !== "") {
+    //     localStorage.setItem("bearerToken", this.securityObject.bearerToken);
+    // }
+    // return of<AppUserAuth>(this.securityObject);
+
+    return this.http.post<AppUserAuth>(API_URL + "login", entity, httpOptions)
+      .pipe(
+        tap(resp => {
+          // Use object assign to update the
+          // current object
+          Object.assign(this.securityObject, resp);
+          
+          // Store into local storage
+          localStorage.setItem("bearerToken",
+              this.securityObject.bearerToken);
+        }));
   }
   
   logout(): void {
