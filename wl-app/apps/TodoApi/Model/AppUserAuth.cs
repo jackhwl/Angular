937a89a017b37f0ead1c5586ca73e38006a30630
Wenlin Huang <jackhwl@hotmail.com>
2020-12-23 22:38:32 -0500

    AppUserAuth class use array of user claims

--- wl-app/apps/TodoApi/Model/AppUserAuth.cs
+++ wl-app/apps/TodoApi/Model/AppUserAuth.cs
@@ -11,15 +11,17 @@ namespace TodoApi.Model
     {
       UserName = "Not authorized";
       BearerToken = string.Empty;
+      Claims = new List<AppUserClaim>();
     }
 
     public string UserName { get; set; }
     public string BearerToken { get; set; }
     public bool IsAuthenticated { get; set; }
-    public bool CanAccessProducts { get; set; }
-    public bool CanAddProduct { get; set; }
-    public bool CanSaveProduct { get; set; }
-    public bool CanAccessCategories { get; set; }
-    public bool CanAddCategory { get; set; }
+    public List<AppUserClaim> Claims { get; set; }
+    //public bool CanAccessProducts { get; set; }
+    //public bool CanAddProduct { get; set; }
+    //public bool CanSaveProduct { get; set; }
+    //public bool CanAccessCategories { get; set; }
+    //public bool CanAddCategory { get; set; }
   }
 }
