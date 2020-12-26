2ad64f9da5b94bec477f1db0291af6140dd04bf5
Wenlin Huang <jackhwl@hotmail.com>
2020-12-26 17:28:18 -0500

    ngrs/data Simplify the Hero, Villain data services

--- ngrx-data-lab/src/app/store/app-store.module.ts
+++ ngrx-data-lab/src/app/store/app-store.module.ts
@@ -3,11 +3,14 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../../environments/environment';
+import { DefaultDataServiceConfig, NgrxDataModule } from 'ngrx-data';
+import { entityConfig } from './entity-metadata';
 
 @NgModule({
   imports: [
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
+    NgrxDataModule.forRoot(entityConfig),
     environment.production ? [] : StoreDevtoolsModule.instrument()
   ]
 })
