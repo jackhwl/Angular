81ba994b8b1c643345d28b38ec77a74244590575
Wenlin Huang <jackhwl@hotmail.com>
2020-12-26 11:32:06 -0500

    Create the NgRx App Store, Define the entities

---
+++ ngrx-data-lab/src/app/store/app-store.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment';
+
+@NgModule({
+  imports: [
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    environment.production ? [] : StoreDevtoolsModule.instrument()
+  ]
+})
+export class AppStoreModule {}
