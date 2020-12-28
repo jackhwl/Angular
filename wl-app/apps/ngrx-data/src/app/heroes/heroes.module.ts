8d067af83cfdf3cafe6c92a022c0028e0223f373
Wenlin Huang <jackhwl@hotmail.com>
2020-12-28 11:25:40 -0500

    add hero detail component

--- wl-app/apps/ngrx-data/src/app/heroes/heroes.module.ts
+++ wl-app/apps/ngrx-data/src/app/heroes/heroes.module.ts
@@ -4,13 +4,21 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { MaterialModule } from '@wl/material';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HeroesComponent }
 ];
 
 @NgModule({
-  imports: [CommonModule, MaterialModule, RouterModule.forChild(routes)],
-  declarations: [HeroesComponent, HeroListComponent]
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  declarations: [HeroesComponent, HeroListComponent, HeroDetailComponent]
 })
 export class HeroesModule {}
