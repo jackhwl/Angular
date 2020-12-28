debc257dfb9a3005fdbcdd3bd8210690b0215602
Wenlin Huang <jackhwl@hotmail.com>
2020-12-28 13:02:21 -0500

    add villain list & detail page

--- wl-app/apps/ngrx-data/src/app/villains/villains.module.ts
+++ wl-app/apps/ngrx-data/src/app/villains/villains.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@wl/material';
 import { VillainsComponent } from './villains/villains.component';
 import { RouterModule, Routes } from '@angular/router';
+import { VillainDetailComponent } from './villain-detail/villain-detail.component';
+import { VillainListComponent } from './villain-list/villain-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: VillainsComponent }
 ];
 
 @NgModule({
-  declarations: [VillainsComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)]
+  declarations: [
+    VillainsComponent,
+    VillainDetailComponent,
+    VillainListComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
+  ]
 })
 export class VillainsModule {}
--- wl-app/apps/ngrx-data/src/app/villains/villains/villains.component.ts
+++ wl-app/apps/ngrx-data/src/app/villains/villains/villains.component.ts
@@ -1,12 +1,88 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { Villain } from '@wl/api-interfaces';
+import { VillainService } from '@wl/core-data';
 
 @Component({
-  selector: 'wl-villains',
+  selector: 'app-villains',
   templateUrl: './villains.component.html',
   styleUrls: ['./villains.component.scss']
 })
 export class VillainsComponent implements OnInit {
-  constructor() {}
+  selected: Villain;
+  villains: Villain[];
+  loading: boolean;
+  // villains$: Observable<Villain[]>;
+  // loading$: Observable<boolean>;
 
-  ngOnInit(): void {}
+  constructor(private villainService: VillainService) {
+    // this.villains$ = villainService.entities$;
+    // this.loading$ = villainService.loading$;
+  }
+
+  ngOnInit() {
+    this.getVillains();
+  }
+
+  add(villain: Villain) {
+    //this.villainService.add(villain);
+    this.loading = true;
+    this.villainService
+      .add(villain)
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe(
+        addedvillain => (this.villains = this.villains.concat(addedvillain))
+      );
+  }
+
+  close() {
+    this.selected = null;
+  }
+
+  delete(villain: Villain) {
+    //this.villainService.delete(villain);
+    //this.close();
+    this.loading = true;
+    this.close();
+    this.villainService
+      .delete(villain)
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe(
+        () => (this.villains = this.villains.filter(h => h.id !== villain.id))
+      );
+  }
+
+  enableAddMode() {
+    this.selected = <any>{};
+  }
+
+  getVillains() {
+    // this.villainService.getAll();
+    // this.close();
+    this.loading = true;
+    this.villainService
+      .getAll()
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe(villains => (this.villains = villains));
+    this.close();
+  }
+
+  select(villain: Villain) {
+    this.selected = villain;
+  }
+
+  update(villain: Villain) {
+    //this.villainService.update(villain);
+    this.loading = true;
+    this.villainService
+      .update(villain)
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe(
+        () =>
+          (this.villains = this.villains.map(h =>
+            h.id === villain.id ? villain : h
+          ))
+      );
+  }
 }
