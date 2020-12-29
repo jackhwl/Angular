064358682a7244caf1455b990b1435dc62597b9d
Wenlin Huang <jackhwl@hotmail.com>
2020-12-29 18:28:49 -0500

    update enviorment

--- wl-app/libs/core-state/src/lib/app-store.module.ts
+++ wl-app/libs/core-state/src/lib/app-store.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-//import { environment } from '../../environments/environment';
+import { environment } from '@env/environment';
 import { DefaultDataServiceConfig, NgrxDataModule } from 'ngrx-data';
 import { entityConfig } from './entity-metadata';
 import { NgrxDataToastService } from './ngrx-data-toast.service';
@@ -12,8 +12,7 @@ import { NgrxDataToastService } from './ngrx-data-toast.service';
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
     NgrxDataModule.forRoot(entityConfig),
-    //environment.production
-    false ? [] : StoreDevtoolsModule.instrument()
+    environment.production ? [] : StoreDevtoolsModule.instrument()
   ]
 })
 export class AppStoreModule {
