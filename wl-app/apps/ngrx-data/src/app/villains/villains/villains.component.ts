6aa9ee38c99ca64d085dc58fe772203c0e0a7e06
Wenlin Huang <jackhwl@hotmail.com>
2020-12-29 13:45:11 -0500

    all variable is observable in component

--- wl-app/apps/ngrx-data/src/app/villains/villains/villains.component.ts
+++ wl-app/apps/ngrx-data/src/app/villains/villains/villains.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { finalize } from 'rxjs/operators';
 import { Villain } from '@wl/api-interfaces';
-import { VillainService, VillainFacade } from '@wl/core-data';
+import { VillainFacade } from '@wl/core-data';
 
 @Component({
   selector: 'app-villains',
@@ -10,22 +9,24 @@ import { VillainService, VillainFacade } from '@wl/core-data';
   styleUrls: ['./villains.component.scss']
 })
 export class VillainsComponent implements OnInit {
-  selected: Villain;
-  //villains: Villain[];
-  //loading: boolean;
+  selectedVillain$: Observable<Villain> = this.villainFacade.selectedVillain$;
   villains$: Observable<Villain[]> = this.villainFacade.villains$;
   loading$: Observable<boolean> = this.villainFacade.loading$;
 
-  constructor(
-    private villainService: VillainService,
-    private villainFacade: VillainFacade
-  ) {
-    // this.villains$ = villainService.entities$;
-    // this.loading$ = villainService.loading$;
-  }
+  constructor(private villainFacade: VillainFacade) {}
 
   ngOnInit() {
+    this.reset();
+    this.villainFacade.mutations$.subscribe(_ => this.reset());
+  }
+
+  reset() {
     this.getVillains();
+    this.selectVillain(null);
+  }
+
+  selectVillain(villain: Villain) {
+    this.villainFacade.selectVillain(villain);
   }
 
   add(villain: Villain) {
@@ -40,30 +41,31 @@ export class VillainsComponent implements OnInit {
     //   );
   }
 
-  close() {
-    this.selected = null;
-  }
+  // close() {
+  //   this.selected = null;
+  // }
 
   delete(villain: Villain) {
-    //this.villainService.delete(villain);
-    //this.close();
-    this.loading = true;
-    this.close();
-    this.villainService
-      .delete(villain)
-      .pipe(finalize(() => (this.loading = false)))
-      .subscribe(
-        () => (this.villains = this.villains.filter(h => h.id !== villain.id))
-      );
+    this.villainFacade.delete(villain);
+    // //this.villainService.delete(villain);
+    // //this.close();
+    // this.loading = true;
+    // this.close();
+    // this.villainService
+    //   .delete(villain)
+    //   .pipe(finalize(() => (this.loading = false)))
+    //   .subscribe(
+    //     () => (this.villains = this.villains.filter(h => h.id !== villain.id))
+    //   );
   }
 
   enableAddMode() {
-    this.selected = <any>{};
+    this.selectedVillain$ = <any>{};
   }
 
   getVillains() {
     this.villainFacade.getAll();
-    this.close();
+    //this.close();
     // // this.villainService.getAll();
     // // this.close();
     // this.loading = true;
@@ -74,21 +76,18 @@ export class VillainsComponent implements OnInit {
     // this.close();
   }
 
-  select(villain: Villain) {
-    this.selected = villain;
-  }
-
   update(villain: Villain) {
-    //this.villainService.update(villain);
-    this.loading = true;
-    this.villainService
-      .update(villain)
-      .pipe(finalize(() => (this.loading = false)))
-      .subscribe(
-        () =>
-          (this.villains = this.villains.map(h =>
-            h.id === villain.id ? villain : h
-          ))
-      );
+    this.villainFacade.update(villain);
+    // //this.villainService.update(villain);
+    // this.loading = true;
+    // this.villainService
+    //   .update(villain)
+    //   .pipe(finalize(() => (this.loading = false)))
+    //   .subscribe(
+    //     () =>
+    //       (this.villains = this.villains.map(h =>
+    //         h.id === villain.id ? villain : h
+    //       ))
+    //   );
   }
 }
