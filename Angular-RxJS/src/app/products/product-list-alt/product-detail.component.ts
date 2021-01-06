58cf2c431314362978154337854d3535ee276485
Wenlin Huang <jackhwl@hotmail.com>
2021-01-05 20:25:31 -0500

    pageTitle observable

--- Angular-RxJS/src/app/products/product-list-alt/product-detail.component.ts
+++ Angular-RxJS/src/app/products/product-list-alt/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { EMPTY } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import { Product } from '../product';
 
 import { ProductService } from '../product.service';
 
@@ -10,7 +11,6 @@ import { ProductService } from '../product.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductDetailComponent {
-  pageTitle = 'Product Detail';
   errorMessage = '';
 
   product$ = this.productService.selectedProduct$.pipe(
@@ -18,6 +18,10 @@ export class ProductDetailComponent {
       this.errorMessage = err;
       return EMPTY;
     })
+  );
+
+  pageTitle$ = this.product$.pipe(
+    map((p: Product) => (p ? `Product Detail for: ${p.productName}` : null))
   );
 
   productSuppliers$ = this.productService.selectedProductSuppliers$.pipe(
