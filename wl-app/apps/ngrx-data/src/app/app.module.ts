5091816bf911f9be37fa7109cdf0af852c5c0a3c
Wenlin Huang <jackhwl@hotmail.com>
2021-01-10 19:16:50 -0500

    add i18n to ngrx-data project

--- wl-app/apps/ngrx-data/src/app/app.module.ts
+++ wl-app/apps/ngrx-data/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '@wl/material';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { RoutingModule } from './routing.module';
 import { ModalComponent } from './modal/modal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppStoreModule } from '@wl/core-state';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [AppComponent, ToolbarComponent, ModalComponent],
@@ -17,9 +19,21 @@ import { AppStoreModule } from '@wl/core-state';
     RoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    AppStoreModule
+    AppStoreModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
