2a9d4ab248147c4b17f51d7ff9ccc28a2213a5b7
Wenlin Huang <jackhwl@hotmail.com>
2021-01-11 09:43:44 -0500

    add inMemoryData in environment

--- wl-app/libs/environments/environment.prod.ts
+++ wl-app/libs/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiEndpoint: 'http://localhost:3000',
-  ngrxData: true
+  ngrxData: true,
+  inMemorryData: false
 };
--- wl-app/libs/environments/environment.ts
+++ wl-app/libs/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiEndpoint: 'http://localhost:3000',
-  ngrxData: true
+  ngrxData: true,
+  inMemorryData: false
 };
