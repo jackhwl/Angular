c44ca71216dcc1f67974d25bb06f45f4a8294a83
Wenlin Huang <jackhwl@hotmail.com>
2021-01-10 22:34:51 -0500

    add in-memorry-db support to recruit

--- wl-app/apps/recruit/src/main.ts
+++ wl-app/apps/recruit/src/main.ts
@@ -3,11 +3,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from '@env/environment';
+import { AppDevModule } from './app/app-dev.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+  //.bootstrapModule(AppModule)
+  .bootstrapModule(AppDevModule)
+  .catch(err => console.error(err));
