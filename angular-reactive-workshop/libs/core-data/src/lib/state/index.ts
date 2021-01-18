2585abea1d839cd7419ce01a8f243222f200f216
Wenlin Huang <jackhwl@hotmail.com>
2021-01-17 21:36:20 -0500

    Combine Reducers

--- angular-reactive-workshop/libs/core-data/src/lib/state/index.ts
+++ angular-reactive-workshop/libs/core-data/src/lib/state/index.ts
@@ -1,23 +1,30 @@
-import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
+import {
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector
+} from '@ngrx/store';
 
 import * as fromCustomers from './customers/customers.reducer';
+import * as fromProjects from './projects/projects.reducer';
 
 export interface AppState {
-  customers: fromCustomers.CustomersState
+  customers: fromCustomers.CustomersState;
+  projects: fromProjects.ProjectsState;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-  customers: fromCustomers.customersReducer
+  customers: fromCustomers.customersReducer,
+  projects: fromProjects.projectsReducers
 };
 
 // -------------------------------------------------------------------
 // CUSTOMERS SELECTORS
 // -------------------------------------------------------------------
-export const selectCustomersState = createFeatureSelector<fromCustomers.CustomersState>('customers');
+export const selectCustomersState = createFeatureSelector<
+  fromCustomers.CustomersState
+>('customers');
 
 export const selectAllCustomers = createSelector(
   selectCustomersState,
   fromCustomers.selectAllCustomers
 );
-
-
