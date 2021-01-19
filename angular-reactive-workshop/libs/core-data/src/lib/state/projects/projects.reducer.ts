5aacce69a8d0c57c160adb2ece0d2db876e5f08e
Wenlin Huang <jackhwl@hotmail.com>
2021-01-19 09:26:48 -0500

    Entities Using NgRx entity

--- angular-reactive-workshop/libs/core-data/src/lib/state/projects/projects.reducer.ts
+++ angular-reactive-workshop/libs/core-data/src/lib/state/projects/projects.reducer.ts
@@ -1,3 +1,4 @@
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
 import { Project } from './../../projects/project.model';
 import { ProjectsActionTypes } from './projects.actions';
 
@@ -36,38 +37,26 @@ const updateProject = (projects, project) =>
 const deleteProject = (projects, project) =>
   projects.filter(w => project.id !== w.id);
 
-export interface ProjectsState {
-  projects: Project[];
+export interface ProjectsState extends EntityState<Project> {
   selectedProjectId: string | null;
 }
 
-export const initialState: ProjectsState = {
-  projects: initialProjects,
+export const adapter: EntityAdapter<Project> = createEntityAdapter<Project>();
+
+export const initialState: ProjectsState = adapter.getInitialState({
   selectedProjectId: null
-};
+});
 
 export function projectsReducers(state = initialState, action): ProjectsState {
   switch (action.type) {
     case ProjectsActionTypes.ProjectSelected:
-      return {
-        selectedProjectId: action.payload,
-        projects: state.projects
-      };
+      return Object.assign({}, state, { selectedProjectId: action.payload });
     case ProjectsActionTypes.AddProject:
-      return {
-        selectedProjectId: state.selectedProjectId,
-        projects: createProject(state.projects, action.payload)
-      };
+      return adapter.addOne(action.payload, state);
     case ProjectsActionTypes.UpdateProject:
-      return {
-        selectedProjectId: state.selectedProjectId,
-        projects: updateProject(state.projects, action.payload)
-      };
+      return adapter.updateOne(action.payload, state);
     case ProjectsActionTypes.DeleteProject:
-      return {
-        selectedProjectId: state.selectedProjectId,
-        projects: deleteProject(state.projects, action.payload)
-      };
+      return adapter.removeOne(action.payload, state);
     default:
       return state;
   }
