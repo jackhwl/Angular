79ac287403d3836192f80c92180e02059bcbda7f
Wenlin Huang <jackhwl@hotmail.com>
2021-01-25 22:11:46 -0500

    add jobs component

--- wl-app/apps/recruit/src/app/jobs/jobs.module.ts
+++ wl-app/apps/recruit/src/app/jobs/jobs.module.ts
@@ -4,13 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '@wl/shared';
 import { JobsListComponent } from './jobs-list/jobs-list.component';
 import { SloganComponent } from './slogan/slogan.component';
+import { JobsComponent } from './jobs/jobs.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: JobsListComponent }
 ];
 
 @NgModule({
-  declarations: [JobsListComponent, SloganComponent],
+  declarations: [JobsListComponent, SloganComponent, JobsComponent],
   imports: [SharedModule, RouterModule.forChild(routes)]
 })
 export class JobsModule {}
---
+++ wl-app/apps/recruit/src/app/jobs/jobs/jobs.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wl-jobs',
+  templateUrl: './jobs.component.html',
+  styleUrls: ['./jobs.component.scss']
+})
+export class JobsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
