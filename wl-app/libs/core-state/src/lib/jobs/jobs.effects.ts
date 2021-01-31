5740047d4baf989e9ecce428151d5f6ba2c2b506
Wenlin Huang <jackhwl@hotmail.com>
2021-01-31 13:46:01 -0500

    jobs effects works!

--- wl-app/libs/core-state/src/lib/jobs/jobs.effects.ts
+++ wl-app/libs/core-state/src/lib/jobs/jobs.effects.ts
@@ -6,27 +6,23 @@ import * as fromJobs from './jobs.reducer';
 import * as JobsActions from './jobs.actions';
 import { JobService } from '@wl/core-data';
 import { Job } from '@wl/api-interfaces';
-import { finalize, map } from 'rxjs/operators';
+import { finalize, map, mergeMap } from 'rxjs/operators';
 
 @Injectable()
 export class JobsEffects {
   loadJobs$ = createEffect(() =>
     this.actions$.pipe(
       ofType(JobsActions.loadJobs),
-      fetch({
-        run: action => {
-          this.jobService.getAll().pipe(
-            map((jobs: Job[]) => {
-              console.log('sss');
-              return JobsActions.loadJobsSuccess({ jobs });
-            })
-          );
-        },
+      // mergeMap(() => this.jobService.getAll().pipe(
+      //   map((jobs: Job[]) => JobsActions.loadJobsSuccess({ jobs }))
+      // ))
 
-        onError: (action, error) => {
-          console.error('Error', error);
-          return JobsActions.loadJobsFailure({ error });
-        }
+      fetch({
+        run: action =>
+          this.jobService
+            .getAll()
+            .pipe(map((jobs: Job[]) => JobsActions.loadJobsSuccess({ jobs }))),
+        onError: (action, error) => JobsActions.loadJobsFailure({ error })
       })
     )
   );
