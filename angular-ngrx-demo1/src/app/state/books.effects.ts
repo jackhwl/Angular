699bd73e266e23753566d566b1843c25a50774ef
Wenlin Huang <jackhwl@hotmail.com>
2021-01-31 13:14:13 -0500

    split mergeMap to map & mergeAll

--- angular-ngrx-demo1/src/app/state/books.effects.ts
+++ angular-ngrx-demo1/src/app/state/books.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { createEffect, Actions, ofType } from "@ngrx/effects";
 import { EMPTY } from "rxjs";
-import { map, mergeMap, catchError, tap } from "rxjs/operators";
+import { map, mergeMap, catchError, tap, mergeAll } from "rxjs/operators";
 import { GoogleBooksService } from "../book-list/books.service";
 import * as BooksActions from "./books.actions";
 
@@ -10,13 +10,21 @@ export class BooksEffects {
   loadBooks$ = createEffect(() =>
     this.actions$.pipe(
       ofType(BooksActions.getBookList),
-      mergeMap(() =>
+      // mergeMap(() =>
+      //   this.bookService.getBooks().pipe(
+      //     tap(books => console.log("aaa=", books)),
+      //     map(books => BooksActions.retrievedBookList({ books })),
+      //     catchError(() => EMPTY)
+      //   )
+      // )
+      map(() =>
         this.bookService.getBooks().pipe(
           tap(books => console.log("aaa=", books)),
           map(books => BooksActions.retrievedBookList({ books })),
           catchError(() => EMPTY)
         )
-      )
+      ),
+      mergeAll()
     )
   );
 
