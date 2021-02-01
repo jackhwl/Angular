2f40ab7b58ae9a51df85a7dd13452d1b30ebea08
Wenlin Huang <jackhwl@hotmail.com>
2021-02-01 17:26:55 -0500

    add selector to fizzbuzz

--- ngrx-fizzbuzz/src/app/reducers/fizzbuzz.reducer.ts
+++ ngrx-fizzbuzz/src/app/reducers/fizzbuzz.reducer.ts
@@ -1,34 +1,29 @@
-import { Action, createReducer, on } from '@ngrx/store';
-
-export const fizzbuzzFeatureKey = 'fizzbuzz';
+import {
+  FizzBuzzActions,
+  FizzBuzzActionTypes
+} from '../actions/fizzbuzz.actions';
 
 export interface State {
   counter: number;
-  message: string;
 }
 
 export const initialState: State = {
-  counter: 1,
-  message: ''
+  counter: 1
 };
 
-export function reducer(state = initialState, action: Action): State {
+export function reducer(state = initialState, action: FizzBuzzActions): State {
   switch (action.type) {
-    case 'NEXT':
-      const counter = state.counter + 1;
-      let message = '';
-      if (counter % 3 === 0) {
-        message += 'Fizz';
-      }
-      if (counter % 5 === 0) {
-        message += 'Buzz';
-      }
+    case FizzBuzzActionTypes.Next:
       return {
-        counter,
-        message: message || counter.toString()
+        counter: state.counter + 1
       };
+
     default:
       return state;
   }
 }
+
+export const getCounter = (state: State) => state.counter;
+
+//export const fizzbuzzFeatureKey = 'fizzbuzz';
 //export const reducer = createReducer(initialState);
--- ngrx-fizzbuzz/src/app/reducers/index.ts
+++ ngrx-fizzbuzz/src/app/reducers/index.ts
@@ -1,4 +1,9 @@
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import {
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import * as fromFizzbuzz from './fizzbuzz.reducer';
 
@@ -13,3 +18,26 @@ export const reducers: ActionReducerMap<State> = {
 export const metaReducers: MetaReducer<State>[] = !environment.production
   ? []
   : [];
+
+export const getFizzBuzzState = createFeatureSelector<fromFizzbuzz.State>(
+  'fizzbuzz'
+);
+
+export const getCounter = createSelector(
+  getFizzBuzzState,
+  fromFizzbuzz.getCounter
+);
+
+export const getMessage = createSelector(
+  getCounter,
+  counter => {
+    let message = '';
+    if (counter % 3 === 0) {
+      message += 'Fizz';
+    }
+    if (counter % 5 === 0) {
+      message += 'Buzz';
+    }
+    return message || counter.toString();
+  }
+);
