99cabe0f7f838dd17c14be4812badf374e7902ce
Wenlin Huang <jackhwl@hotmail.com>
2021-02-04 20:18:49 -0500

    login-form component split

---
+++ wl-app/apps/recruit/src/app/auth/components/login-form/login-form.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Credentials } from '@wl/api-interfaces';
+
+@Component({
+  selector: 'bc-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent implements OnInit {
+  @Input()
+  set pending(isPending: boolean) {
+    if (isPending) {
+      this.form.disable();
+    } else {
+      this.form.enable();
+    }
+  }
+
+  @Input() errorMessage: string | null;
+
+  @Output() submitted = new EventEmitter<Credentials>();
+
+  form: FormGroup = new FormGroup({
+    username: new FormControl('ngrx'),
+    password: new FormControl('')
+  });
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  submit() {
+    if (this.form.valid) {
+      this.submitted.emit(this.form.value);
+    }
+  }
+}
