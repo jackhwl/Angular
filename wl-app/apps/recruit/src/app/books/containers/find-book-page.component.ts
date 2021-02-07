6c861152f291e414f3a443010be0fa0ce5a0abce
Wenlin Huang <jackhwl@hotmail.com>
2021-02-07 13:07:34 -0500

    add books containers folder

---
+++ wl-app/apps/recruit/src/app/books/containers/find-book-page.component.ts
@@ -0,0 +1,43 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+import * as fromBooks from '../../books/reducers';
+import { FindBookPageActions } from '../actions';
+import { Book } from '../models/book';
+
+@Component({
+  selector: 'bc-find-book-page',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <bc-book-search
+      [query]="searchQuery$ | async"
+      [searching]="loading$ | async"
+      [error]="error$ | async"
+      (search)="search($event)"
+    >
+    </bc-book-search>
+    <bc-book-preview-list [books]="books$ | async"> </bc-book-preview-list>
+  `
+})
+export class FindBookPageComponent {
+  searchQuery$: Observable<string>;
+  books$: Observable<Book[]>;
+  loading$: Observable<boolean>;
+  error$: Observable<string>;
+
+  constructor(private store: Store<fromBooks.State>) {
+    this.searchQuery$ = store.pipe(
+      select(fromBooks.getSearchQuery),
+      take(1)
+    );
+    this.books$ = store.pipe(select(fromBooks.getSearchResults));
+    this.loading$ = store.pipe(select(fromBooks.getSearchLoading));
+    this.error$ = store.pipe(select(fromBooks.getSearchError));
+  }
+
+  search(query: string) {
+    this.store.dispatch(FindBookPageActions.searchBooks({ query }));
+  }
+}
