2ce239f644670723b4b42c98e6fc2e62a9bf7466
Wenlin Huang <jackhwl@hotmail.com>
2021-02-08 13:34:51 -0500

    sharedModule

--- wl-app/apps/recruit/src/app/books/books.module.ts
+++ wl-app/apps/recruit/src/app/books/books.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
+import { SharedModule } from '@wl/shared';
+
 import { reducers } from '../books/reducers';
 import { BooksComponentsModule } from './components';
 import { BooksRoutingModule } from './books-routing.module';
@@ -14,6 +16,7 @@ import { CollectionPageComponent } from './containers/collection-page.component'
 
 @NgModule({
   imports: [
+    SharedModule,
     BooksComponentsModule,
     BooksRoutingModule,
 
