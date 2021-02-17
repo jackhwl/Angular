bedac34676f08567be79c7ddb1adc9c674a508fa
Wenlin Huang <jackhwl@hotmail.com>
2021-02-17 11:52:18 -0500

    move store to coreModule

--- workspace/apps/tiny-app/src/app/app.module.ts
+++ workspace/apps/tiny-app/src/app/app.module.ts
@@ -2,23 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '@workspace/shared/environments';
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers
-    }),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-      logOnly: environment.production
-    })
-  ],
+  imports: [BrowserModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
