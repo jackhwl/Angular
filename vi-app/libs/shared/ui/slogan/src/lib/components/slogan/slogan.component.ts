2068e43cfb0016e42b50905b537fa41410d680ed
Wenlin Huang <jackhwl@hotmail.com>
2021-02-22 21:41:55 -0500

    add slogan component

---
+++ vi-app/libs/shared/ui/slogan/src/lib/components/slogan/slogan.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vi-slogan',
+  templateUrl: './slogan.component.html',
+  styleUrls: ['./slogan.component.scss']
+})
+export class SloganComponent {}
