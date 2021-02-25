94b6dd9493c9c76ba813cf069c2e3eece4a96a28
Wenlin Huang <jackhwl@hotmail.com>
2021-02-24 20:45:33 -0500

    add books lib

--- vi-app/jest.config.js
+++ vi-app/jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
     '<rootDir>/libs/shared/ui/slogan',
     '<rootDir>/libs/shared/auth',
     '<rootDir>/libs/api-interfaces',
-    '<rootDir>/libs/shared/common'
+    '<rootDir>/libs/shared/common',
+    '<rootDir>/libs/books'
   ]
 };
