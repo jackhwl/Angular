2c9cdbafec791cb0a11d6b02beb011a665208c5b
Wenlin Huang <jackhwl@hotmail.com>
2021-03-02 09:26:05 -0500

    original angular router project(2.0)

---
+++ router_mailapp/src/main.ts
@@ -0,0 +1,13 @@
+import "./polyfills.ts";
+
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+
+import { MailModule } from "./app/";
+import { environment } from "./environments/environment";
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(MailModule);
