f4543a2c1ab2c20bfd2d906f7db1e05adc9bd8c6
Wenlin Huang <jackhwl@hotmail.com>
2021-03-06 13:21:36 -0500

    all services of villain

---
+++ vi-app/libs/villains/src/lib/services/villain.facade.ts
@@ -0,0 +1,108 @@
+import { Injectable, Injector } from '@angular/core';
+import { environment } from '@vi/shared/environments';
+import { Villain } from '../models/villain';
+
+import { BehaviorSubject, pipe } from 'rxjs';
+import { finalize, tap } from 'rxjs/operators';
+import { VillainNgrxDataService } from './villain.ngrx.data.service';
+import { VillainService } from './villain.service';
+import { ToastService } from './toast.service';
+
+@Injectable()
+export class VillainFacade {
+  private mutations = new BehaviorSubject(null);
+  private loading = new BehaviorSubject<boolean>(true);
+  private villains = new BehaviorSubject<Villain[]>(null);
+  private selectedVillain = new BehaviorSubject<Villain>(null);
+
+  mutations$ = this.mutations.asObservable();
+  loading$ = this.loading.asObservable();
+  villains$ = this.villains.asObservable();
+  selectedVillain$ = this.selectedVillain.asObservable();
+  nfs = {
+    getAll: {
+      i18n_key: 'i18.villains.villains_retrieved_successfully',
+      action: 'GET'
+    },
+    add: {
+      i18n_key: 'i18.villains.villain_created',
+      action: 'POST'
+    },
+    update: {
+      i18n_key: 'i18.villains.villain_updated',
+      action: 'PUT'
+    },
+    delete: {
+      i18n_key: 'i18.villains.villain_deleted',
+      action: 'DELETE'
+    }
+  };
+  villainService: VillainNgrxDataService | VillainService;
+  constructor(private injector: Injector, private toastService: ToastService) {
+    this.villainService = environment.ngrxData
+      ? <VillainNgrxDataService>this.injector.get(VillainNgrxDataService)
+      : <VillainService>this.injector.get(VillainService);
+  }
+
+  notification(type: string, name: string, cb?: any) {
+    cb
+      ? cb()
+      : this.toastService.open(this.nfs[type].i18n_key, this.nfs[type].action, {
+          name
+        });
+  }
+
+  villainFinalize = (type: string, name: string, cb?: any) =>
+    pipe(
+      finalize(() => this.loading.next(false)),
+      tap(() => this.notification(type, name, cb))
+    );
+
+  reset() {
+    this.mutations.next(true);
+  }
+
+  selectVillain(villain: Villain) {
+    this.selectedVillain.next(villain);
+  }
+
+  getAll(cb?: any) {
+    this.loading.next(true);
+    this.villainService
+      .getAll()
+      .pipe(this.villainFinalize('getAll', '', cb))
+      .subscribe((villains: Villain[]) => this.villains.next(villains));
+  }
+
+  saveVillain(villain: Villain) {
+    if (villain.id) {
+      this.update(villain);
+    } else {
+      this.add(villain);
+    }
+  }
+
+  add(villain: Villain, cb?: any) {
+    this.loading.next(true);
+    this.villainService
+      .add(villain)
+      .pipe(this.villainFinalize('add', villain.name, cb))
+      .subscribe(_ => this.reset());
+  }
+
+  update(villain: Villain, cb?: any) {
+    this.loading.next(true);
+    this.villainService
+      .update(villain)
+      .pipe(this.villainFinalize('update', villain.name, cb))
+      .subscribe(_ => this.reset());
+  }
+
+  delete(villain: Villain, cb?: any) {
+    this.loading.next(true);
+    this.villainService
+      .delete(villain)
+      .pipe(this.villainFinalize('delete', villain.name, cb))
+      .subscribe(_ => this.reset());
+  }
+}
---
+++ vi-app/libs/villains/src/lib/services/villain.service.ts
@@ -0,0 +1,71 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Location } from '@angular/common';
+
+import { Villain } from '../models/villain';
+import { environment } from '@vi/shared/environments';
+import { ErrorService } from './error.service';
+
+@Injectable({ providedIn: 'root' })
+export class VillainService {
+  model = 'villains';
+  constructor(
+    private http: HttpClient,
+    private location: Location,
+    private errorService: ErrorService
+  ) {}
+
+  getUrl() {
+    const endpoint = environment.inMemorryData
+      ? 'api'
+      : environment.apiEndpoint;
+    const api = this.location.normalize(endpoint);
+    return `${api}/${this.model}`;
+  }
+
+  getUrlForId(id) {
+    return `${this.getUrl()}/${id}`;
+  }
+
+  // logout() {
+  //   return this.http.get(`${api}/logout`);
+  // }
+
+  // getProfile() {
+  //   return this.http.get<any>(`${api}/profile`);
+  // }
+
+  // getVillain(id: number) {
+  //   return this.http.get<Array<Villain>>(this.getUrlForId(id)).pipe(
+  //     map(villain => villain),
+  //     tap(() =>
+  //       this.toastService.openSnackBar('Villain retrieved successfully!', 'GET')
+  //     ),
+  //     this.errorService.catchReThrowError()
+  //   );
+  // }
+
+  getAll() {
+    return this.http
+      .get<Villain[]>(this.getUrl())
+      .pipe(this.errorService.retryAfter());
+  }
+
+  add(villain: Villain) {
+    return this.http
+      .post<Villain>(this.getUrl(), villain)
+      .pipe(this.errorService.retryAfter());
+  }
+
+  update(villain: Villain) {
+    return this.http
+      .put<Villain>(this.getUrlForId(villain.id), villain)
+      .pipe(this.errorService.retryAfter());
+  }
+
+  delete(villain: Villain) {
+    return this.http
+      .delete(this.getUrlForId(villain.id))
+      .pipe(this.errorService.retryAfter());
+  }
+}
