cbd3973db6432ab870527053c3cfb66f3132f595
Wenlin Huang <jackhwl@hotmail.com>
2021-03-06 19:40:58 -0500

    ngrx/data EntityDataModule

---
+++ vi-app/apps/recruit/src/app/core/entity-metadata.ts
@@ -0,0 +1,15 @@
+import { EntityMetadataMap } from '@ngrx/data';
+
+const entityMetadata: EntityMetadataMap = {
+  Student: {},
+  Hero: {},
+  Villain: {}
+};
+
+// because the plural of "hero" is not "heros"
+const pluralNames = { Hero: 'Heroes' };
+
+export const entityConfig = {
+  entityMetadata,
+  pluralNames
+};
