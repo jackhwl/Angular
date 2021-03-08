abfeb43b489a152c9f859f9d3f093de6ec159d4d
Wenlin Huang <jackhwl@hotmail.com>
2021-03-08 17:10:43 -0500

    students CRUD action

--- vi-app/libs/students/src/lib/actions/students-api.actions.ts
+++ vi-app/libs/students/src/lib/actions/students-api.actions.ts
@@ -1,9 +1,9 @@
 import { createAction, props } from '@ngrx/store';
-import { Student as StudentsEntity } from '../models/student';
+import { Student } from '../models/student';
 
 export const loadStudentsSuccess = createAction(
   '[Students/API] Load Students Success',
-  props<{ students: StudentsEntity[] }>()
+  props<{ students: Student[] }>()
 );
 
 export const loadStudentsFailure = createAction(
@@ -14,4 +14,34 @@ export const loadStudentsFailure = createAction(
 export const notifyLoadStudentsSuccess = createAction(
   '[Toastr Notification/API] Display Load Students Success',
   props<{ title: string; description: string; interpolateParams?: Object }>()
+);
+
+export const createStudentSuccess = createAction(
+  '[Students/API] Create Student Success',
+  props<{ student: Student }>()
+);
+
+export const createStudentFailure = createAction(
+  '[Students/API] Create Student Failure',
+  props<{ error: any }>()
+);
+
+export const updateStudentSuccess = createAction(
+  '[Students/API] Update Student Success',
+  props<{ student: Student }>()
+);
+
+export const updateStudentFailure = createAction(
+  '[Students/API] Update Student Failure',
+  props<{ error: any }>()
+);
+
+export const deleteStudentSuccess = createAction(
+  '[Students/API] Delete Student Success',
+  props<{ student: Student }>()
+);
+
+export const deleteStudentFailure = createAction(
+  '[Students/API] Delete Student Failure',
+  props<{ error: any }>()
 );
--- vi-app/libs/students/src/lib/actions/students.actions.ts
+++ vi-app/libs/students/src/lib/actions/students.actions.ts
@@ -1,3 +1,36 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
+import { Student } from '../models/student';
+
+export const selectStudent = createAction(
+  '[Students] Select Student',
+  props<{ selectedId: string }>()
+);
 
 export const loadStudents = createAction('[Students] Load Students');
+
+// export const loadStudent = createAction(
+//   '[Students] Load Student',
+//   props<{ student: Student }>()
+// );
+
+// Create Student
+export const createStudent = createAction(
+  '[Students] Create Student',
+  props<{ student: Student }>()
+);
+
+// Update Student
+export const updateStudent = createAction(
+  '[Students] Update Student',
+  props<{ student: Student }>()
+);
+
+// Delete Student
+export const deleteStudent = createAction(
+  '[Students] Delete Student',
+  props<{ student: Student }>()
+);
+
+export const deleteStudentCancelled = createAction(
+  '[Students] Delete Student Cancelled'
+);
