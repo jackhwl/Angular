e550a33a2e4764903bfebd43a325c677d90d5e8d
Wenlin Huang <jackhwl@hotmail.com>
2021-03-08 17:10:14 -0500

    widget store facade

--- fem-production-angular-boilerplate/libs/core-state/README.md
+++ fem-production-angular-boilerplate/libs/core-state/README.md
@@ -5,3 +5,5 @@ This library was generated with [Nx](https://nx.dev).
 ## Running unit tests
 
 Run `nx test core-state` to execute the unit tests.
+
+https://github.com/onehungrymind/fem-production-angular
---
+++ fem-production-angular-boilerplate/libs/core-state/src/lib/widgets/widgets.store.facade.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { Widget } from '@fem/api-interfaces';
+import { Action, ActionsSubject, select, Store } from '@ngrx/store';
+import { filter } from 'rxjs/operators';
+import * as WidgetsActions from './widgets.actions';
+import * as WidgetsSelectors from './widgets.selectors';
+
+@Injectable()
+export class WidgetsFacade {
+  loaded$ = this.store.pipe(select(WidgetsSelectors.getWidgetsLoaded));
+  allWidgets$ = this.store.pipe(select(WidgetsSelectors.getAllWidgets));
+  selectedWidget$ = this.store.pipe(select(WidgetsSelectors.getSelectedWidget));
+
+  mutations$ = this.actions$.pipe(
+    filter(
+      (action: Action) =>
+        action.type === WidgetsActions.createWidget({} as any).type ||
+        action.type === WidgetsActions.updateWidget({} as any).type ||
+        action.type === WidgetsActions.deleteWidget({} as any).type
+    )
+  );
+
+  constructor(private store: Store, private actions$: ActionsSubject) {}
+
+  selectWidget(selectedId: string) {
+    this.dispatch(WidgetsActions.selectWidget({ selectedId }));
+  }
+
+  loadWidgets() {
+    this.dispatch(WidgetsActions.loadWidgets());
+  }
+
+  loadWidget(widgetId: string) {
+    this.dispatch(WidgetsActions.loadWidget({ widgetId }));
+  }
+
+  saveWidget(widget: Widget) {
+    if (widget.id) {
+      this.updateWidget(widget);
+    } else {
+      this.createWidget(widget);
+    }
+  }
+
+  createWidget(widget: Widget) {
+    this.dispatch(WidgetsActions.createWidget({ widget }));
+  }
+
+  updateWidget(widget: Widget) {
+    this.dispatch(WidgetsActions.updateWidget({ widget }));
+  }
+
+  deleteWidget(widget: Widget) {
+    this.dispatch(WidgetsActions.deleteWidget({ widget }));
+  }
+
+  dispatch(action: Action) {
+    this.store.dispatch(action);
+  }
+}
