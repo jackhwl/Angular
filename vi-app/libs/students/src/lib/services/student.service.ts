3e86d40763d8d7d58faeb8cf42aa093a24e32d49
Wenlin Huang <jackhwl@hotmail.com>
2021-03-12 19:37:09 -0500

    add error handler to all method

--- vi-app/libs/students/src/lib/services/student.service.ts
+++ vi-app/libs/students/src/lib/services/student.service.ts
@@ -4,13 +4,18 @@ import { Location } from '@angular/common';
 
 import { environment } from '@vi/shared/environments';
 import { Student } from '../models/student';
+import { ErrorService } from './error.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
   model = 'students';
-  constructor(private httpClient: HttpClient, private location: Location) {}
+  constructor(
+    private httpClient: HttpClient,
+    private location: Location,
+    private errorService: ErrorService
+  ) {}
 
   getUrl() {
     const endpoint = environment.inMemorryData
@@ -25,22 +30,32 @@ export class StudentService {
   }
 
   all() {
-    return this.httpClient.get<Student[]>(this.getUrl());
+    return this.httpClient
+      .get<Student[]>(this.getUrl())
+      .pipe(this.errorService.retryAfter());
   }
 
   find(studentId) {
-    return this.httpClient.get<Student>(this.getUrlForId(studentId));
+    return this.httpClient
+      .get<Student>(this.getUrlForId(studentId))
+      .pipe(this.errorService.retryAfter());
   }
 
   create(student) {
-    return this.httpClient.post(this.getUrl(), student);
+    return this.httpClient
+      .post(this.getUrl(), student)
+      .pipe(this.errorService.retryAfter());
   }
 
   update(student) {
-    return this.httpClient.put(this.getUrlForId(student.id), student);
+    return this.httpClient
+      .put(this.getUrlForId(student.id), student)
+      .pipe(this.errorService.retryAfter());
   }
 
   delete(studentId) {
-    return this.httpClient.delete(this.getUrlForId(studentId));
+    return this.httpClient
+      .delete(this.getUrlForId(studentId))
+      .pipe(this.errorService.retryAfter());
   }
 }
