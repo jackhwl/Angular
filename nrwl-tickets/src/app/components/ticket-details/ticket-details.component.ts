b1ffb6ca04abf78a29a821d4076b818c02640090
Wenlin Huang <jackhwl@hotmail.com>
2021-03-18 22:37:33 -0400

    methods added to ticket details component

--- nrwl-tickets/src/app/components/ticket-details/ticket-details.component.ts
+++ nrwl-tickets/src/app/components/ticket-details/ticket-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { TicketsFacade } from "src/app/services";
 import { Ticket } from "../../services/backend.service";
 
 @Component({
@@ -6,19 +8,37 @@ import { Ticket } from "../../services/backend.service";
   templateUrl: "./ticket-details.component.html",
   styleUrls: ["./ticket-details.component.scss"]
 })
-export class TicketDetailsComponent {
+export class TicketDetailsComponent implements OnInit {
   currentTicket: Ticket;
-  @Input() set ticket(value: Ticket) {
-    this.currentTicket = { ...value };
+  id: string;
+  constructor(
+    private ticketsFacade: TicketsFacade,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    route.params.subscribe(_ => (this.id = _.id));
   }
-  @Output() cancelled = new EventEmitter();
-  @Output() saved = new EventEmitter();
 
-  resetForm(form) {
-    form.reset();
-    // form.controls.forEach((name, control) => {
-    //   control.updateValue('');
-    //   control.setErrors(null);
-    // });
+  saved(ticket) {
+    if (ticket.id !== null) {
+      this.ticketsFacade.updateTicket(ticket);
+    } else {
+      this.ticketsFacade.createTicket(ticket);
+    }
+    this.router.navigate(["tickets"]);
+  }
+  cancelled() {}
+
+  ngOnInit(): void {
+    this.ticketsFacade.selectedTicket$.subscribe(
+      tickets => (this.currentTicket = { ...tickets })
+    );
+    if (this.id !== null) {
+      this.selectTicketById(this.id === "new" ? null : this.id);
+    }
+  }
+
+  selectTicketById(id: string) {
+    this.ticketsFacade.selectTicketById(id);
   }
 }
