242fc2758f9fdef97e2afa6d578679b7fb56d414
Wenlin Huang <jackhwl@hotmail.com>
2021-03-30 18:46:53 -0400

    use reactive form

--- nrwl-tickets/src/app/containers/tickets/tickets.component.ts
+++ nrwl-tickets/src/app/containers/tickets/tickets.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
 import { Router } from "@angular/router";
 import { interval, Observable, Subject, Subscription } from "rxjs";
-import { debounce, map } from "rxjs/operators";
+import { debounce, debounceTime, map } from "rxjs/operators";
 import { TicketsFacade } from "../../services";
 
 @Component({
@@ -13,19 +14,24 @@ export class TicketsComponent implements OnInit, OnDestroy {
   q$: Observable<string> = this.ticketsFacade.q$;
   error$: Observable<string | null> = this.ticketsFacade.error$;
   subject = new Subject<string>();
+  qbSub: Subscription | undefined;
   querySub: Subscription | undefined;
   mutationSub: Subscription | undefined;
+  qb = new FormControl("");
 
   constructor(private ticketsFacade: TicketsFacade, private router: Router) {}
 
   ngOnInit() {
+    this.qbSub = this.q$.subscribe(_ => this.qb.setValue(_));
     this.mutationSub = this.ticketsFacade.mutations$.subscribe(_ => {
+      console.log("motation");
       this.ticketsFacade.loadUsers();
       this.ticketsFacade.loadFilterTicketsByRoute();
     });
-    this.querySub = this.subject
+
+    this.querySub = this.qb.valueChanges
       .pipe(
-        debounce(() => interval(200)),
+        debounceTime(200),
         map((q: string) =>
           this.router.navigate(["tickets"], {
             queryParams: { q },
@@ -36,11 +42,16 @@ export class TicketsComponent implements OnInit, OnDestroy {
       .subscribe();
   }
 
-  query(q: string) {
-    this.subject.next(q);
-  }
+  // query(q: string) {
+  //   this.router.navigate(["tickets"], {
+  //     queryParams: { q },
+  //     queryParamsHandling: "merge"
+  //   })
+  //   //this.subject.next(q);
+  // }
 
   ngOnDestroy() {
+    if (this.qbSub) this.qbSub.unsubscribe();
     if (this.querySub) this.querySub.unsubscribe();
     if (this.mutationSub) this.mutationSub.unsubscribe();
   }
