4f1eda639d48e14b01b6922dfb304e9dbe4db531
Wenlin Huang <jackhwl@hotmail.com>
2021-05-01 18:45:54 -0400

    use ng-add to run schematics with option setup

--- schematics/hello2/src/callsca/index.ts
+++ schematics/hello2/src/callsca/index.ts
@@ -2,9 +2,12 @@ import {
   Rule,
   SchematicContext,
   Tree,
-  chain
+  chain,
+  SchematicsException
   //externalSchematic
 } from "@angular-devkit/schematics";
+import { join, normalize } from "path";
+import { getWorkspace } from "@schematics/angular/utility/workspace";
 
 const licenseText = `
 /**
@@ -18,28 +21,44 @@ const licenseText = `
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function callsca(_options: any): Rule {
-  console.log("init _options=", _options);
-  return chain([
-    //externalSchematic("@schematics/angular", "component", _options),
-    (tree: Tree, _context: SchematicContext) => {
-      console.log("_options.sourceDir=", _options.sourceDir);
-      tree.getDir(_options.sourceDir).visit(filePath => {
-        console.log("filePath=", filePath);
-        if (!filePath.endsWith(".ts")) {
-          return;
-        }
-        const content = tree.read(filePath);
-        if (!content) {
-          return;
-        }
 
-        // Prevent from writing license to files that already have one.
-        if (content.indexOf(licenseText) == -1) {
-          tree.overwrite(filePath, licenseText + content);
-        }
-      });
-      return tree;
-    }
-  ]);
+export async function setupOptions(host: Tree, options: any): Promise<Tree> {
+  const workspace = await getWorkspace(host);
+  if (!options.project) {
+    options.project = workspace.projects.keys().next().value;
+  }
+  const project = workspace.projects.get(options.project);
+  if (!project) {
+    throw new SchematicsException(`Invalid project name: ${options.project}`);
+  }
+
+  options.path = join(normalize(project.root), "src", "app");
+  return host;
+}
+
+export function callsca(_options: any): Rule {
+  return async (tree: Tree, _context: SchematicContext) => {
+    await setupOptions(tree, _options);
+    return chain([
+      //externalSchematic("@schematics/angular", "component", _options),
+      (tree: Tree, _context: SchematicContext) => {
+        tree.getDir(_options.path).visit(filePath => {
+          if (!filePath.endsWith(".ts")) {
+            return;
+          }
+          const content = tree.read(filePath);
+          if (!content) {
+            return;
+          }
+
+          // Prevent from writing license to files that already have one.
+          if (content.indexOf(licenseText) == -1) {
+            tree.overwrite(filePath, licenseText + content);
+          }
+        });
+
+        return tree;
+      }
+    ]);
+  };
 }
