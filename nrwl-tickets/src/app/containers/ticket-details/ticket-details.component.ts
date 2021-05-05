9ad44b7ae1967d6be1354e6560765acc0be1e2e7
Wenlin Huang <jackhwl@hotmail.com>
2021-05-05 19:47:20 -0400

    remove facade from ticket-detail component

--- nrwl-tickets/src/app/containers/ticket-details/ticket-details.component.ts
+++ nrwl-tickets/src/app/containers/ticket-details/ticket-details.component.ts
@@ -2,8 +2,12 @@ import { ChangeDetectionStrategy } from "@angular/core";
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
-import { TicketsFacade } from "../../services";
+import { select, Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { TicketsActions } from "src/app/actions";
 import { Ticket } from "../../services/backend.service";
+import * as TicketsSelectors from "../../reducers/tickets.selectors";
+import * as UsersSelectors from "../../reducers/users.selectors";
 
 @Component({
   selector: "vi-ticket-details",
@@ -12,7 +16,7 @@ import { Ticket } from "../../services/backend.service";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TicketDetailsComponent implements OnInit {
-  users$ = this.ticketsFacade.allUsers$;
+  users$ = this.store.pipe(select(UsersSelectors.getAllUsers));
   detailForm = this.fb.group({
     title: [""],
     id: [""],
@@ -20,15 +24,17 @@ export class TicketDetailsComponent implements OnInit {
     completed: ["", Validators.required],
     description: ["", Validators.required]
   });
-
+  selectedTicketByRoute$: Observable<Ticket> = this.store.pipe(
+    select(TicketsSelectors.getSelectedByRoute)
+  );
   constructor(
     private fb: FormBuilder,
-    private ticketsFacade: TicketsFacade,
+    private store: Store<{}>,
     private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.ticketsFacade.selectedTicketByRoute$.subscribe((ticket: Ticket) =>
+    this.selectedTicketByRoute$.subscribe((ticket: Ticket) =>
       this.detailForm.patchValue({
         ...ticket,
         title: ticket.id === null ? "New Ticket" : "Edit Ticket"
@@ -43,14 +49,22 @@ export class TicketDetailsComponent implements OnInit {
   onSubmit(): void {
     const ticket = this.detailForm.value as Ticket;
     if (ticket.id !== null && ticket.id !== undefined) {
-      this.ticketsFacade.updateTicket(ticket);
+      this.updateTicket(ticket);
     } else {
-      this.ticketsFacade.createTicket(ticket);
+      this.createTicket(ticket);
     }
     this.router.navigate(["tickets"], { queryParamsHandling: "merge" });
   }
 
   cancelled(): void {
     this.router.navigate(["tickets"], { queryParamsHandling: "merge" });
+  }
+
+  createTicket(ticket: Ticket): void {
+    this.store.dispatch(TicketsActions.createTicket({ ticket }));
+  }
+
+  updateTicket(ticket: Ticket): void {
+    this.store.dispatch(TicketsActions.updateTicket({ ticket }));
   }
 }
