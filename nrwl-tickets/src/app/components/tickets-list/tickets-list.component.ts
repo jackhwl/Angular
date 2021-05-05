8f1e428edf7d4b54ba92dcb9da27978975ec951c
Wenlin Huang <jackhwl@hotmail.com>
2021-05-05 18:57:39 -0400

    remove facade from ticketlist component

--- nrwl-tickets/src/app/components/tickets-list/tickets-list.component.ts
+++ nrwl-tickets/src/app/components/tickets-list/tickets-list.component.ts
@@ -1,6 +1,12 @@
 import { ChangeDetectionStrategy } from "@angular/core";
 import { Component } from "@angular/core";
-import { TicketsFacade } from "../../services";
+import { select, Store } from "@ngrx/store";
+import { combineLatest, Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import * as TicketsSelectors from "../../reducers/tickets.selectors";
+import * as UsersSelectors from "../../reducers/users.selectors";
+import { TicketVm } from "src/app/models/ticketvm";
+import { Ticket, User } from "src/app/services/backend.service";
 
 @Component({
   selector: "vi-tickets-list",
@@ -9,7 +15,28 @@ import { TicketsFacade } from "../../services";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TicketsListComponent {
-  loaded$ = this.ticketsFacade.loaded$;
-  tickets$ = this.ticketsFacade.allTicketVms$;
-  constructor(private ticketsFacade: TicketsFacade) {}
+  loaded$ = this.store.pipe(select(TicketsSelectors.getLoaded));
+
+  allTickets$: Observable<Ticket[]> = this.store.pipe(
+    select(TicketsSelectors.getAllTickets)
+  );
+
+  allUsers$: Observable<User[]> = this.store.pipe(
+    select(UsersSelectors.getAllUsers)
+  );
+
+  tickets$ = this.getAllTicketVms();
+
+  constructor(private store: Store<{}>) {}
+
+  getAllTicketVms(): Observable<TicketVm[]> {
+    return combineLatest([this.allTickets$, this.allUsers$]).pipe(
+      map(([tickets, users]) =>
+        tickets.map((ticket: TicketVm) => ({
+          ...ticket,
+          assignees: users.filter(user => user.id === ticket.assigneeId)
+        }))
+      )
+    );
+  }
 }
