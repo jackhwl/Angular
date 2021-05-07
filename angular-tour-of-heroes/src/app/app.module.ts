e57a0c5b6dc6d64de14915160a56761407800241
Wenlin Huang <jackhwl@hotmail.com>
2021-05-07 14:58:56 -0400

    Create a feature component

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
