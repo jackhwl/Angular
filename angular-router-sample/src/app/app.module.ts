e4dd24e97d9805cb5197687735e15c536af190b3
Wenlin Huang <jackhwl@hotmail.com>
2021-05-11 08:41:00 -0400

    Milestone 2: Routing module

--- angular-router-sample/src/app/app.module.ts
+++ angular-router-sample/src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'heroes', component: HeroesListComponent },
-  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
-];
 
 @NgModule({
   declarations: [
@@ -21,14 +16,7 @@ const appRoutes: Routes = [
     HeroesListComponent,
     PageNotFoundComponent
   ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
-  ],
-  providers: [],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
