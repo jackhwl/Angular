0bef6c6a386aaf16cb0e470137b60c2f70066887
Wenlin Huang <jackhwl@hotmail.com>
2021-05-16 18:33:03 -0400

    CanActivateChild: guarding child routes

---
+++ angular-router-sample/src/app/admin/manage-crises/manage-crises.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ManageCrisesComponent } from './manage-crises.component';
+
+describe('ManageCrisesComponent', () => {
+  let component: ManageCrisesComponent;
+  let fixture: ComponentFixture<ManageCrisesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ManageCrisesComponent]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ManageCrisesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- angular-router-sample/src/app/app.module.ts
+++ angular-router-sample/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,18 +8,17 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesModule } from './heroes/heroes.module';
-import { CrisisCenterModule } from './crisis-center/crisis-center.module';
 import { ComposeMessageComponent } from './compose-message/compose-message.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent, ComposeMessageComponent],
   imports: [
-    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     HeroesModule,
-    CrisisCenterModule,
+    AuthModule,
     AppRoutingModule
   ],
   bootstrap: [AppComponent]
