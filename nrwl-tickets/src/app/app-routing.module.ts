a5c740afa86ab1f2dc2f07e079dc0ff1bafb4da3
Wenlin Huang <jackhwl@hotmail.com>
2021-06-11 09:12:11 -0400

    no need to import AppComponent in

--- nrwl-tickets/src/app/app-routing.module.ts
+++ nrwl-tickets/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AppComponent } from "./containers/app.component";
+//import { AppComponent } from "./containers/app.component";
 
 const routes: Routes = [
   {
     path: "tickets",
     loadChildren: () => import("./tickets.module").then(m => m.TicketsModule)
   },
-  { path: "", redirectTo: "tickets", pathMatch: "full" },
-  { path: "**", component: AppComponent }
+  { path: "", redirectTo: "tickets", pathMatch: "full" }
+  //  { path: "**", component: AppComponent }
 ];
 
 @NgModule({
