a54f4303f5b07f15b4092755633ba67f307a1ebe
Wenlin Huang <jackhwl@hotmail.com>
2021-06-21 09:03:45 -0400

    add Exponential backoff to error service

--- wl-app/libs/core-data/src/lib/error/error.service.ts
+++ wl-app/libs/core-data/src/lib/error/error.service.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { iif, Observable, of, throwError, timer } from 'rxjs';
+import { iif, Observable, of, throwError, timer, pipe, zip, range } from 'rxjs';
 import {
   catchError,
   concatMap,
@@ -8,13 +8,28 @@ import {
   delayWhen,
   retryWhen,
   take,
-  tap
+  tap,
+  mergeMap,
+  map
 } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class ErrorService {
   catchReThrowError() {
     return catchError(this.handleError);
+  }
+
+  //https://angular.io/guide/practical-observable-usage#exponential-backoff
+  backoff(delay: number = 250, maxTries: number = 3) {
+    return pipe(
+      retryWhen(attempts =>
+        zip(range(1, maxTries + 1), attempts).pipe(
+          mergeMap(([i, err]) => (i > maxTries ? throwError(err) : of(i))),
+          map(i => i * i),
+          mergeMap(v => timer(v * delay))
+        )
+      )
+    );
   }
 
   retryAfter(delayTime: number = 1000, stop: number = 3) {
