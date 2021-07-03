fc348a8ded48d3a51a6d482adf2d1341390e18b2
Wenlin Huang <jackhwl@hotmail.com>
2021-07-03 16:44:46 -0400

    CHECK FOR TRUTHY & FALSY VALUES

--- Jest/functions.js
+++ Jest/functions.js
@@ -1,5 +1,12 @@
 const functions = {
-  add: (num1, num2) => num1 + num2
+  add: (num1, num2) => num1 + num2,
+  isNull: () => null,
+  checkValue: x => x,
+  createUser: () => {
+    const user = { firstName: "Jack" };
+    user["lastName"] = "Huang";
+    return user;
+  }
 };
 
 module.exports = functions;
--- Jest/functions.test.js
+++ Jest/functions.test.js
@@ -3,3 +3,44 @@ const functions = require("./functions");
 test("Adds 2 + 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+test("Adds 2 + 2 to NOT equal 5", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+test("Should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+test("Should be falsy", () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+test("User should be Jack Huang object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Jack",
+    lastName: "Huang"
+  });
+});
+
+test("Should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 800;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+
+test("There is no I in team", () => {
+  expect("team").not.toMatch(/I/i);
+});
+
+test("Admin should be in usernames", () => {
+  usernames = ["john", "karen", "admin"];
+  expect(usernames).toContain("admin");
+});
