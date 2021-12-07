85bdf70bdfe542625cf43c58b514b03fa49fb2a3
Wenlin Huang <jackhwl@hotmail.com>
2021-12-06 21:52:03 -0500

    Lab 4 - Generate a component lib

--- nrwl-ngconf/apps/store/src/app/app.module.ts
+++ nrwl-ngconf/apps/store/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 
 import { MatCardModule } from '@angular/material/card';
+import { StoreSharedUiModule } from '@nrwl-ngconf/store/shared-ui';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     MatCardModule,
+    StoreSharedUiModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })
   ],
   providers: [],
---
+++ nrwl-ngconf/libs/store/shared-ui/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/store-shared-ui.module';
