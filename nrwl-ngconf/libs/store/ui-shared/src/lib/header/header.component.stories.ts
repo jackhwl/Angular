72314d84b102cbb88f810b5e52a0a67f0c4e8058
Wenlin Huang <jackhwl@hotmail.com>
2021-12-09 18:20:05 -0500

    Lab 10 - Generate Storybook stories for the shared ui component

---
+++ nrwl-ngconf/libs/store/ui-shared/src/lib/header/header.component.stories.ts
@@ -0,0 +1,23 @@
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { HeaderComponent } from './header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+export default {
+  title: 'HeaderComponent',
+  component: HeaderComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [MatToolbarModule]
+    })
+  ]
+} as Meta<HeaderComponent>;
+
+const Template: Story<HeaderComponent> = (args: HeaderComponent) => ({
+  component: HeaderComponent,
+  props: args
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  title: ''
+};
