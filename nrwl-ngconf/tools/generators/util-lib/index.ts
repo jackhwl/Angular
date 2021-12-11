029c23c6f413bf8ab6689009c0082efccd82d801
Wenlin Huang <jackhwl@hotmail.com>
2021-12-11 15:21:29 -0500

    Lab 13 - Workspace Generators - Intro step 10

--- nrwl-ngconf/tools/generators/util-lib/index.ts
+++ nrwl-ngconf/tools/generators/util-lib/index.ts
@@ -1,10 +1,14 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
-export default async function(tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
-  await formatFiles(tree);
+export default async function(host: Tree, schema: any) {
+  await libraryGenerator(host, {
+    name: `util-${schema.name}`,
+    directory: schema.directory,
+    tags: `type:util, scope:${schema.directory}`
+  });
+  await formatFiles(host);
   return () => {
-    installPackagesTask(tree);
+    installPackagesTask(host);
   };
 }
