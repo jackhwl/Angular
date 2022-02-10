e39ea3f9c32d12a16567d3f62f8dedfae1d416f6
Wenlin Huang <jackhwl@hotmail.com>
2022-02-09 20:36:12 -0500

    first jest test

--- nrwl-tickets/src/app/containers/app.component.spec.ts
+++ nrwl-tickets/src/app/containers/app.component.spec.ts
@@ -1,27 +1,22 @@
-import { TestBed, async } from "@angular/core/testing";
+import {render, screen, fireEvent} from '@testing-library/angular'
+import { async } from 'rxjs';
 import { AppComponent } from "./app.component";
 describe("AppComponent", () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [AppComponent]
-    }).compileComponents();
-  }));
-  it("should create the app", async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("app");
-  }));
-  it("should render title in a h1 tag", async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector("h1").textContent).toContain(
-      "Welcome to app!"
-    );
-  }));
+  it("should create the app", async () => {
+    await render(AppComponent);
+    expect(screen.getByText('Run all tests')).toBeInTheDocument();
+  });
+//   xit(`should have as title 'app'`, async () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.debugElement.componentInstance;
+//     expect(app.title).toEqual("app");
+//   });
+//   xit("should render title in a h1 tag", async () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     fixture.detectChanges();
+//     const compiled = fixture.debugElement.nativeElement;
+//     expect(compiled.querySelector("h1").textContent).toContain(
+//       "Welcome to app!"
+//     );
+//   });
 });
