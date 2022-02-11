c526351625c7d24de2b2f1713089b7c3c984a289
Wenlin Huang <jackhwl@hotmail.com>
2022-02-11 13:47:01 -0500

    use ByRole instead of ByText

--- nrwl-tickets/src/app/containers/ticket-list/ticket-list.component.spec.ts
+++ nrwl-tickets/src/app/containers/ticket-list/ticket-list.component.spec.ts
@@ -7,8 +7,6 @@ import { TicketListComponent } from './ticket-list.component';
 import * as TicketsVmSelectors from "../../reducers/ticket-vm.selectors";
 import * as TicketsSelectors from "../../reducers/ticket.selectors";
 import { MaterialModule } from 'src/app/material.module';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { APP_BASE_HREF } from '@angular/common';
 
@@ -28,30 +26,33 @@ const tickets: Ticket_vm[] = [{
   addresses: [],
   assignees: []
 }];
+
+const providers = [
+  {provide: APP_BASE_HREF, useValue: '/'},
+  provideMockStore({ 
+    selectors: [{
+      selector: TicketsVmSelectors.getFilterTicketsVmByRoute,
+      value: tickets
+    },
+    {
+      selector: TicketsSelectors.getLoaded,
+      value: true
+    }
+  ]
+  })
+]
+
 describe('TicketListComponent', () => {
   it("should render the list", async () => {
     await render(TicketListComponent, {
         imports: [MaterialModule, RouterTestingModule],
-        providers: [
-          {provide: APP_BASE_HREF, useValue: '/'},
-          provideMockStore({ 
-            selectors: [{
-              selector: TicketsVmSelectors.getFilterTicketsVmByRoute,
-              value: tickets
-            },
-            {
-              selector: TicketsSelectors.getLoaded,
-              value: true
-            }
-          ]
-          })
-        ]
+        providers
     });
 
     const store = TestBed.inject(MockStore);
     store.dispatch = jest.fn();
 
-    expect(screen.getAllByText('Edit')).toHaveLength(tickets.length);
+    expect(screen.getAllByRole('link')).toHaveLength(tickets.length);
   });
 //   xit(`should have as title 'app'`, async () => {
 //     const fixture = TestBed.createComponent(AppComponent);
