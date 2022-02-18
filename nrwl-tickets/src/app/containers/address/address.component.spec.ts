6a21922774998e9d1e0aa1c3fa151debbd0bcedb
Wenlin Huang <jackhwl@hotmail.com>
2022-02-17 19:13:34 -0500

    test phone component exists in address component

--- nrwl-tickets/src/app/containers/address/address.component.spec.ts
+++ nrwl-tickets/src/app/containers/address/address.component.spec.ts
@@ -1,25 +1,76 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from "@angular/forms";
+import { By } from "@angular/platform-browser";
+import { render, screen, fireEvent } from "@testing-library/angular";
+import userEvent from "@testing-library/user-event";
+import { UtilService } from "src/app/services";
 
-import { AddressComponent } from './address.component';
+import { TicketsComponentsModule } from "../ticketsComponentsModule";
+import { AddressComponent } from "./address.component";
+const phoneFormGroup1 = new FormBuilder().group({
+  id: ['1'],
+  type: ['home'],
+  number: ['416-333-4455']
+})
+const phoneFormGroup2 = new FormBuilder().group({
+  id: ['2'],
+  type: ['mobile'],
+  number: ['647-443-1345']
+})
+const formGroup = new FormBuilder().group({
+  id: ['a1'],
+  addr1: ['3050 del mar ave'],
+  addr2: ['aaa'],
+  postcode: ['91770'],
+  phones: new FormBuilder().array([
+    phoneFormGroup1,
+    phoneFormGroup2
+  ])
+})
 
 describe('AddressComponent', () => {
-  let component: AddressComponent;
-  let fixture: ComponentFixture<AddressComponent>;
+  async function setup(formGroup, index = 3, spy = jest.fn()) {
+    const container = await render(AddressComponent, {
+      imports: [TicketsComponentsModule],
+      providers: [
+        { provide: UtilService, useValue: new UtilService(new FormBuilder())}
+      ],
+      componentProperties: { 
+        formGroup,
+        index,
+        deleteAddress: {
+          emit: spy
+        } as any
+      }
+    });
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AddressComponent ]
-    })
-    .compileComponents();
+    return { container }
+  }
+
+  it("should render the address component", async () => {
+    await setup(formGroup);
+    expect(screen.getByRole('textbox', { name: /address 1/i })).toHaveValue(formGroup.value.addr1.value);
+    expect(screen.getByRole('textbox', { name: /address 2/i })).toHaveValue(formGroup.value.addr2.value);
+    expect(screen.getByRole('textbox', { name: /postal code/i })).toHaveValue(formGroup.value.postcode.value);
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AddressComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('renders phone component', async () => {
+    const { container } = await setup(formGroup);
+    const { debugElement } = container.fixture;
+    const childComponent = debugElement.query(By.css('vi-phone'));
+    expect(childComponent).toBeTruthy();
+    const childComponents = debugElement.queryAll(By.css('vi-phone'));
+    expect(childComponents).toHaveLength(formGroup.value.phones.length);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("should emit index when delete button clicked", async () => {
+    const deleteAddressSpy = jest.fn()
+    const index = 5
+    await setup(formGroup, index, deleteAddressSpy);
+    const btn = screen.getByRole('button', { name: /delete address/i });
+
+    userEvent.click(btn);
+
+    expect(deleteAddressSpy).toHaveBeenCalledWith(index);
   });
+
 });
