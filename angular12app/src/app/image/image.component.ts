89dca93d036f8a6fdc9009d70ccf90284dd9e70f
Wenlin Huang <jackhwl@hotmail.com>
2022-05-27 08:24:11 -0400

    add image component

--- angular12app/src/app/image/image.component.ts
+++ angular12app/src/app/image/image.component.ts
@@ -7,14 +7,17 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ImageComponent implements OnInit {
   @Input()
-  imgSrc: string = '<UPDATE IMAGE URL>';
+  imgSrc: string = '/assets/1.jpeg';
 
   @Input()
   altTxt?: string = 'Pondicherry French Hotel';
 
   @Input()
-  figCaptionTxt?: string = 'The French Quarter or White Town area in Pondicherry is filled with e'
+  figCaptionTxt?: string = 'The French Quarter or White Town area in Pondicherry is filled with elegant colonial mansions in the midst of tree-lined boulevards, named on French streets beginning with "rue", numerous parks and charming cafés.'
   
+  @Input()
+  imgOpacity?: number = 1;
+
   constructor() { }
 
   ngOnInit(): void {
