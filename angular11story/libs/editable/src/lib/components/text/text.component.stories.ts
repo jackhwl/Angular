bd1f789ba3a6304c05c07f2743abd9386f658806
Wenlin Huang <jackhwl@hotmail.com>
2022-05-29 18:46:46 -0400

    storybook 11

--- angular11story/libs/editable/src/lib/components/text/text.component.stories.ts
+++ angular11story/libs/editable/src/lib/components/text/text.component.stories.ts
@@ -1,18 +1,36 @@
 import { text, number, boolean } from '@storybook/addon-knobs';
+import { Story, IStory } from '@storybook/angular'
 import { TextComponent } from './text.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 export default {
-  title: 'TextComponent'
+  title: 'Components/Text Component'
 }
 
-export const primary = () => ({
+const template: Story<TextComponent> = (args: TextComponent): IStory => ({
   moduleMetadata: {
-    imports: []
+    imports: [
+      ReactiveFormsModule,
+    ]
   },
   component: TextComponent,
   props: {
-    backgroundColor: text('backgroundColor', `#D0B0DA`),
-    dataStatus: text('dataStatus', 'saved'),
-    textValue: text('textValue', ''),
+    textValue: 'Initial Value',
+    ...args
   }
 })
+
+export const displaying = template.bind({})
+displaying.args = {
+  state: 'displaying'
+}
+
+export const editing = template.bind({})
+editing.args = {
+  state: 'editing'
+}
+
+export const updating = template.bind({})
+updating.args = {
+  state: 'updating'
+}
