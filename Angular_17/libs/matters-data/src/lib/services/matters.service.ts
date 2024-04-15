58a4d2f36fd5288a976c21185c58a5606f8b51a9
Wenlin Huang <jackhwl@hotmail.com>
2024-04-15 12:40:59 -0700

    Generate matter service

--- Angular_17/libs/matters-data/src/lib/services/matters.service.ts
+++ Angular_17/libs/matters-data/src/lib/services/matters.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Matter } from '@proto/api-interfaces';
+
+// TEMPORARY
+const API_URL = 'http://localhost:3300/api';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MattersService {
+  model = 'matters';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  all() {
+    return this.http.get<Matter[]>(this.getUrl());
+  }
+
+  find(id: string) {
+    return this.http.get<Matter>(this.getUrlWithId(id));
+  }
+
+  create(matter: Matter) {
+    return this.http.post(this.getUrl(), matter);
+  }
+
+  update(matter: Matter) {
+    return this.http.patch(this.getUrlWithId(matter.id), matter);
+  }
+
+  delete(matter: Matter) {
+    return this.http.delete(this.getUrlWithId(matter.id));
+  }
+
+  private getUrl() {
+    return `/${this.model}`;
+  }
+
+  private getUrlWithId(id: string | undefined | null) {
+    return `${this.getUrl()}/`;
+  }
 }
