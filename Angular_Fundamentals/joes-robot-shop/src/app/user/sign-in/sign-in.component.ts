a55ade7d16d9826a8b56d88229ce09362fc68c1d
Wenlin Huang <jackhwl@hotmail.com>
2024-05-29 21:06:29 -0400

    Binding to Form Controls with ngModel

--- Angular_Fundamentals/joes-robot-shop/src/app/user/sign-in/sign-in.component.ts
+++ Angular_Fundamentals/joes-robot-shop/src/app/user/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IUserCredentials } from '../user.model';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bot-sign-in',
@@ -8,4 +10,14 @@ import { IUserCredentials } from '../user.model';
 })
 export class SignInComponent {
   credentials: IUserCredentials = {email: '', password: ''}
+
+  constructor(private userSvc: UserService, private router: Router){}
+
+  signIn() {
+    //console.log('sign-in called', this.credentials)
+    this.router.navigate(['/catalog'])
+    // this.userSvc.signIn(this.credentials).subscribe({
+    //   next: () => this.router.navigate(['/catalog'])
+    // })
+  }
 }
